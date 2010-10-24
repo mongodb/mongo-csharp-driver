9dcbb55b6bd1cd846e162fbb4b065ec82a5b8cdd
craiggwilson <craiggwilson@gmail.com>
2010-10-23 23:51:34 -0400

    Implemented CombGuidGenerator and added unit tests for all the Id generators.

--- Bson/DefaultSerializer/BsonIdGenerators.cs
+++ Bson/DefaultSerializer/BsonIdGenerators.cs
@@ -62,4 +62,41 @@ namespace MongoDB.Bson.DefaultSerializer {
         }
         #endregion
     }
+
+    public class CombGuidGenerator : IBsonIdGenerator
+    {
+        #region constructors
+        public CombGuidGenerator() {
+        }
+        #endregion
+
+        #region public methods
+        public object GenerateId() {
+            var guidArray = Guid.NewGuid().ToByteArray();
+
+            var baseDate = new DateTime(1900, 1, 1);
+            var now = DateTime.Now;
+
+            var days = new TimeSpan(now.Ticks - baseDate.Ticks);
+            var msecs = now.TimeOfDay;
+
+            var daysArray = BitConverter.GetBytes(days.Days);
+            var msecsArray = BitConverter.GetBytes((long)(msecs.TotalMilliseconds));
+
+            Array.Reverse(daysArray);
+            Array.Reverse(msecsArray);
+
+            Array.Copy(daysArray, daysArray.Length - 2, guidArray, guidArray.Length - 6, 2);
+            Array.Copy(msecsArray, msecsArray.Length - 4, guidArray, guidArray.Length - 4, 4);
+
+            return new Guid(guidArray);
+        }
+
+        public bool IsEmpty(
+            object id
+        ) {
+            return (Guid) id == Guid.Empty;
+        }
+        #endregion
+    }
 }
