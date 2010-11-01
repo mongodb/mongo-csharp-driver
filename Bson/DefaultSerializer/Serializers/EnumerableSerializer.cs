e709dd56753a1e6a0a2c5e813c46cefa8ba4db25
rstam <robert@10gen.com>
2010-11-01 12:19:23 -0400

    Split out generic version of EnumerableSerializer into its own file. Added unit tests for EnumerableSerializer<T>.

---
+++ Bson/DefaultSerializer/Serializers/EnumerableSerializer.cs
@@ -0,0 +1,103 @@
+﻿/* Copyright 2010 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDB.Bson.DefaultSerializer {
+    public class EnumerableSerializer : BsonBaseSerializer {
+        #region private static fields
+        private static EnumerableSerializer singleton = new EnumerableSerializer();
+        #endregion
+
+        #region constructors
+        private EnumerableSerializer() {
+        }
+        #endregion
+
+        #region public static properties
+        public static EnumerableSerializer Singleton {
+            get { return singleton; }
+        }
+        #endregion
+
+        #region public static methods
+        public static void RegisterSerializers() {
+            BsonSerializer.RegisterSerializer(typeof(ArrayList), singleton);
+            BsonSerializer.RegisterSerializer(typeof(ICollection), singleton);
+            BsonSerializer.RegisterSerializer(typeof(IEnumerable), singleton);
+            BsonSerializer.RegisterSerializer(typeof(IList), singleton);
+        }
+        #endregion
+
+        #region public methods
+        public override object DeserializeElement(
+            BsonReader bsonReader,
+            Type nominalType,
+            out string name
+        ) {
+            var bsonType = bsonReader.PeekBsonType();
+            if (bsonType == BsonType.Null) {
+                bsonReader.ReadNull(out name);
+                return null;
+            } else if (bsonType == BsonType.Array) {
+                bsonReader.ReadArrayName(out name);
+                bsonReader.ReadStartDocument();
+                var list = new ArrayList();
+                while (bsonReader.HasElement()) {
+                    var elementType = BsonClassMapSerializer.GetActualElementType(bsonReader, typeof(object));
+                    var serializer = BsonSerializer.LookupSerializer(elementType);
+                    string elementName; // elementNames are ignored on input
+                    var element = serializer.DeserializeElement(bsonReader, typeof(object), out elementName);
+                    list.Add(element);
+                }
+                bsonReader.ReadEndDocument();
+                return list;
+            } else {
+                var message = string.Format("Can't deserialize a {0} from BsonType {1}", nominalType.FullName, bsonType);
+                throw new FileFormatException(message);
+            }
+        }
+
+        public override void SerializeElement(
+            BsonWriter bsonWriter,
+            Type nominalType,
+            string name,
+            object value
+        ) {
+            if (value == null) {
+                bsonWriter.WriteNull(name);
+            } else {
+                bsonWriter.WriteArrayName(name);
+                bsonWriter.WriteStartDocument();
+                int index = 0;
+                foreach (var element in (IEnumerable) value) {
+                    var elementName = index.ToString();
+                    BsonSerializer.SerializeElement(bsonWriter, typeof(object), elementName, element);
+                    index++;
+                }
+                bsonWriter.WriteEndDocument();
+            }
+        }
+        #endregion
+    }
+}
---
+++ BsonUnitTests/DefaultSerializer/Serializers/EnumerableSerializerGenericTests.cs
@@ -0,0 +1,210 @@
+﻿/* Copyright 2010 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+using MongoDB.Bson;
+using MongoDB.Bson.DefaultSerializer;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDB.BsonUnitTests.DefaultSerializer.EnumerableSerializerGeneric {
+    [BsonDiscriminator("EnumerableSerializerGeneric.C")] // "C" is an ambiguous discriminator when nominalType is System.Object
+    public class C {
+        public string P { get; set; }
+    }
+
+    [TestFixture]
+    public class EnumerableSerializerTests {
+        public class T {
+            public List<object> L { get; set; }
+            public ICollection<object> IC { get; set; }
+            public IEnumerable<object> IE { get; set; }
+            public IList<object> IL { get; set; }
+        }
+
+        [Test]
+        public void TestNull() {
+            List<object> list = null;
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "null";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsNull(rehydrated.L);
+            Assert.IsNull(rehydrated.IC);
+            Assert.IsNull(rehydrated.IE);
+            Assert.IsNull(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestEmpty() {
+            var list = new List<object>();
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[]";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestOneC() {
+            var list = new List<object>(new[] { new C { P = "x" } });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[{ '_t' : 'EnumerableSerializerGeneric.C', 'P' : 'x' }]";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestOneInt() {
+            var list = new List<object>(new object[] { 1 });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[1]";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestOneString() {
+            var list = new List<object>(new[] { "x" });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "['x']";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestTwoCs() {
+            var list = new List<object>(new[] { new C { P = "x" }, new C { P = "y" } });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[{ '_t' : 'EnumerableSerializerGeneric.C', 'P' : 'x' }, { '_t' : 'EnumerableSerializerGeneric.C', 'P' : 'y' }]";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestTwoInts() {
+            var list = new List<object>(new object[] { 1, 2 });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[1, 2]";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestTwoStrings() {
+            var list = new List<object>(new[] { "x", "y" });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "['x', 'y']";
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Test]
+        public void TestMixedPrimitiveTypes() {
+            var dateTime = DateTime.SpecifyKind(new DateTime(2010, 1, 1, 11, 22, 33), DateTimeKind.Utc);
+            var millis = (long) ((dateTime - BsonConstants.UnixEpoch).TotalMilliseconds);
+            var guid = Guid.Empty;
+            var objectId = ObjectId.Empty;
+            var list = new List<object>(new object[] { true, dateTime, 1.5, 1, 2L, guid, objectId, "x" });
+            var obj = new T { L = list, IC = list, IE = list, IL = list };
+            var json = obj.ToJson();
+            var rep = "[true, #Date, 1.5, 1, 2, #Guid, #ObjectId, 'x']";
+            rep = rep.Replace("#Date", "{ '$date' : #ms }".Replace("#ms", millis.ToString()));
+            rep = rep.Replace("#Guid", "{ '$binary' : 'AAAAAAAAAAAAAAAAAAAAAA==', '$type' : '03' }");
+            rep = rep.Replace("#ObjectId", "{ '$oid' : '000000000000000000000000' }");
+            var expected = "{ 'L' : #R, 'IC' : #R, 'IE' : #R, 'IL' : #R }".Replace("#R", rep).Replace("'", "\"");
+            Assert.AreEqual(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.DeserializeDocument<T>(bson);
+            Assert.IsInstanceOf<List<object>>(rehydrated.L);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IC);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IE);
+            Assert.IsInstanceOf<List<object>>(rehydrated.IL);
+            Assert.IsTrue(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+    }
+}
