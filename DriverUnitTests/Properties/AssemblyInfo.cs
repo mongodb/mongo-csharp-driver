7e0e76bbf55b7a4ae5a649dca80a9309c4ef69e1
Justin Dearing <zippy1981@gmail.com>
2010-11-28 09:05:20 -0500

    CSHARP-72 Unify common assembly attributes into GlobalAssembly.cs
    Moved all common attributes to GlobalAssembly.cs.

--- DriverUnitTests/Properties/AssemblyInfo.cs
+++ DriverUnitTests/Properties/AssemblyInfo.cs
@@ -23,28 +23,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MongoDB.DriverUnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("10gen Inc.")]
-[assembly: AssemblyProduct("MongoDB.DriverUnitTests")]
-[assembly: AssemblyCopyright("Copyright © 2010 10gen Inc.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d1d7aede-4d9c-407d-959a-118b166fcf3f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.3959")]
