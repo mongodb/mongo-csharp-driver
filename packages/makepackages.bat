e8f12d7dc25b89d4796615ee1cf49ede81f68183
rstam <robert@10gen.com>
2011-01-25 15:47:37 -0500

    Minor changes related to upcoming v0.11 release (new version number etc...).

--- packages/makepackages.bat
+++ packages/makepackages.bat
@@ -1,4 +1,4 @@
-set VER=0.9.0.3992
+set VER=0.11.0.4042
 set PACKAGES=packages-%VER%
 
 echo Creating %PACKAGES%
@@ -12,7 +12,7 @@ ren %PACKAGES%\CSharpDriver.msi CSharpDriver-%VER%.msi
 set ZIPEXE="C:\Program Files\7-Zip\7z.exe"
 set ZIPFILE=%PACKAGES%\CSharpDriver-%VER%.zip
 %ZIPEXE% a %ZIPFILE% ..\License.txt
-%ZIPEXE% a %ZIPFILE% "..\Release Notes v0.9.txt"
+%ZIPEXE% a %ZIPFILE% "..\Release Notes v0.11.txt"
 %ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.dll
 %ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Driver.dll
 
