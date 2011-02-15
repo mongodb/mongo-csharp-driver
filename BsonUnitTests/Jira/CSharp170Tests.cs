ef588c20bd648727f3668ee6223572f1383ca01e
rstam <robert@10gen.com>
2011-02-15 15:46:58 -0500

    Added unit tests for CSHARP-170.

---
+++ BsonUnitTests/Jira/CSharp170Tests.cs
@@ -0,0 +1,50 @@
+﻿/* Copyright 2010-2011 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using NUnit.Framework;
+
+using MongoDB.Bson;
+using MongoDB.Bson.DefaultSerializer;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+
+namespace MongoDB.BsonUnitTests.Jira.CSharp170 {
+    [TestFixture]
+    public class CSharp170Tests {
+        public class C {
+            public Collection<int> Collection;
+            public ObservableCollection<int> Observable;
+        }
+
+        [Test]
+        public void TestDeserializeDouble() {
+            var obj = new C {
+                Collection = new Collection<int> { 1, 2, 3 },
+                Observable = new ObservableCollection<int> { 1, 2, 3 }
+            };
+            var json = obj.ToJson();
+            var expected = "{ Collection : [1, 2, 3], Observable : [1, 2, 3] }";
+            Assert.AreEqual(expected, json);
+        }
+    }
+}
