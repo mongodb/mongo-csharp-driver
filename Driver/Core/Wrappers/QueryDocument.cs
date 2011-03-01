b9c0bfc59d3cbd8be6ae7dbcd34d98b676933df5
rstam <robert@10gen.com>
2011-03-01 17:55:00 -0500

    Added doc comments to CollectionOptionsDocument, CommandDocument, FieldsDocument, GeoNearOptionsDocument, GroupByDocument,IndexKeysDocument, IndexOptionsDocument, MapReduceOptionsDocument, QueryDocument, ScopeDocument, SortByDocument and UpdateDocument.

--- Driver/Core/Wrappers/QueryDocument.cs
+++ Driver/Core/Wrappers/QueryDocument.cs
@@ -23,29 +23,53 @@ using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
 
 namespace MongoDB.Driver {
+    /// <summary>
+    /// Represents a BSON document that can be used where an IMongoQuery is expected.
+    /// </summary>
     public class QueryDocument : BsonDocument, IMongoQuery {
         #region constructors
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class.
+        /// </summary>
         public QueryDocument() {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class specifying whether duplicate element names are allowed
+        /// (allowing duplicate element names is not recommended).
+        /// </summary>
+        /// <param name="allowDuplicateNames">Whether duplicate element names are allowed.</param>
         public QueryDocument(
             bool allowDuplicateNames
         )
             : base(allowDuplicateNames) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and adds one element.
+        /// </summary>
+        /// <param name="element">An element to add to the document.</param>
         public QueryDocument(
             BsonElement element
         )
             : base(element) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and adds new elements from a dictionary of key/value pairs.
+        /// </summary>
+        /// <param name="dictionary">A dictionary to initialize the document from.</param>
         public QueryDocument(
             IDictionary<string, object> dictionary
         )
             : base(dictionary) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and adds new elements from a dictionary of key/value pairs.
+        /// </summary>
+        /// <param name="dictionary">A dictionary to initialize the document from.</param>
+        /// <param name="keys">A list of keys to select values from the dictionary.</param>
         public QueryDocument(
             IDictionary<string, object> dictionary,
             IEnumerable<string> keys
@@ -53,18 +77,31 @@ namespace MongoDB.Driver {
             : base(dictionary, keys) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and adds new elements from a list of elements.
+        /// </summary>
+        /// <param name="elements">A list of elements to add to the document.</param>
         public QueryDocument(
             IEnumerable<BsonElement> elements
         )
             : base(elements) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and adds one or more elements.
+        /// </summary>
+        /// <param name="elements">One or more elements to add to the document.</param>
         public QueryDocument(
             params BsonElement[] elements
         )
             : base(elements) {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the QueryDocument class and creates and adds a new element.
+        /// </summary>
+        /// <param name="name">The name of the element to add to the document.</param>
+        /// <param name="value">The value of the element to add to the document.</param>
         public QueryDocument(
             string name,
             BsonValue value
