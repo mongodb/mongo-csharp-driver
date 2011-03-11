10e72f0538dd656fc5c28483cf464a0d409f588b
rstam <robert@10gen.com>
2011-03-10 19:55:20 -0500

    Fixed minor bug in ReplicaSetConnector.cs (exception was being thrown if Servers was not of type List).

--- Driver/Internal/ReplicaSetConnector.cs
+++ Driver/Internal/ReplicaSetConnector.cs
@@ -151,8 +151,8 @@ namespace MongoDB.Driver.Internal {
 
         private BlockingQueue<QueryNodeResponse> QueueSeedListQueries() {
             var responseQueue = new BlockingQueue<QueryNodeResponse>();
-            var addresses = (List<MongoServerAddress>) server.Settings.Servers;
-            var endPoints = (List<IPEndPoint>) server.EndPoints;
+            var addresses = server.Settings.Servers.ToList();
+            var endPoints = server.EndPoints.ToList();
             for (int i = 0; i < addresses.Count; i++) {
                 var args = new QueryNodeParameters {
                     Address = addresses[i],
