973e87bd2083bdab154d4429e40625a09c4f11c4
rstam <robert@10gen.com>
2011-05-11 10:44:07 -0400

    Merged in pull request 59 from Justin Dearing to allow passing a Hashtable to the constructor of a QueryDocument or an UpdateDocument. Made equivalent changes to the rest of the classes that derive from BsonDocument. These new constructors facilitate creating these objects from Powershell.

--- Driver/Core/Wrappers/ScopeDocument.cs
+++ Driver/Core/Wrappers/ScopeDocument.cs
@@ -14,6 +14,7 @@
 */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -73,6 +74,28 @@ namespace MongoDB.Driver {
         public ScopeDocument(
             IDictionary<string, object> dictionary,
             IEnumerable<string> keys
+        )
+            : base(dictionary, keys) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ScopeDocument class and adds new elements from a dictionary of key/value pairs.
+        /// </summary>
+        /// <param name="dictionary">A dictionary to initialize the document from.</param>
+        public ScopeDocument(
+            IDictionary dictionary
+        )
+            : base(dictionary) {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ScopeDocument class and adds new elements from a dictionary of key/value pairs.
+        /// </summary>
+        /// <param name="dictionary">A dictionary to initialize the document from.</param>
+        /// <param name="keys">A list of keys to select values from the dictionary.</param>
+        public ScopeDocument(
+            IDictionary dictionary,
+            IEnumerable keys
         )
             : base(dictionary, keys) {
         }
