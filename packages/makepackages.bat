896666bda25c2bc8530781e07a428417c071e057
rstam <robert@10gen.com>
2011-06-16 12:29:16 -0400

    Changing setup project for version 1.1.

--- packages/makepackages.bat
+++ packages/makepackages.bat
@@ -1,4 +1,4 @@
-set VER=1.0.1.1234
+set VER=1.1.0.0
 set PACKAGES=packages-%VER%
 
 echo Creating %PACKAGES%
@@ -12,7 +12,7 @@ ren %PACKAGES%\CSharpDriver.msi CSharpDriver-%VER%.msi
 set ZIPEXE="C:\Program Files\7-Zip\7z.exe"
 set ZIPFILE=%PACKAGES%\CSharpDriver-%VER%.zip
 %ZIPEXE% a %ZIPFILE% ..\License.txt
-%ZIPEXE% a %ZIPFILE% "..\Release Notes v1.0.txt"
+%ZIPEXE% a %ZIPFILE% "..\Release Notes v1.1.txt"
 %ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.dll
 %ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.pdb
 %ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.xml
