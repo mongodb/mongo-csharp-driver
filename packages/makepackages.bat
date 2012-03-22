8ddb10dbc84aa0d527588245b808555890be4d22
rstam <robert@10gen.com>
2012-03-22 16:40:35 -0400

    Worked on CSHARP-256. Replaced Debug with Release as necessary. Changed Release config of project files to build XML documentation.

--- packages/makepackages.bat
+++ packages/makepackages.bat
@@ -6,19 +6,19 @@ pause
 
 rmdir /s /q %PACKAGES%
 mkdir %PACKAGES%
-xcopy /f /d /y ..\DriverSetup\Debug\CSharpDriver.msi %PACKAGES%\
+xcopy /f /d /y ..\DriverSetup\Release\CSharpDriver.msi %PACKAGES%\
 ren %PACKAGES%\CSharpDriver.msi CSharpDriver-%VER%.msi
 
 set ZIPEXE="C:\Program Files\7-Zip\7z.exe"
 set ZIPFILE=%PACKAGES%\CSharpDriver-%VER%.zip
 %ZIPEXE% a %ZIPFILE% ..\License.txt
 %ZIPEXE% a %ZIPFILE% "..\Release Notes v1.3.1.txt"
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.dll
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.pdb
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Bson.xml
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Driver.dll
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Driver.pdb
-%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Debug\MongoDB.Driver.xml
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Bson.dll
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Bson.pdb
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Bson.xml
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Driver.dll
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Driver.pdb
+%ZIPEXE% a %ZIPFILE% ..\Driver\bin\Release\MongoDB.Driver.xml
 %ZIPEXE% a %ZIPFILE% ..\Help\CSharpDriverDocs.chm
 
 echo Created %PACKAGES%
