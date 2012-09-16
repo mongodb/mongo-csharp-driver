13ffde78547df956c41281ab8e9a43a9bd8c0f13
Justin Dearing <zippy1981@gmail.com>
2012-09-15 23:07:14 -0400

    Implements CSHARP-572: PowerShell Unit tests for the driver and a unit test for CSHARP-570.

---
+++ DriverUnitTestsPosh/Jira/CSharp570Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MonogoDB.DriverUnitTestsPosh;
+using NUnit.Framework;
+
+namespace DriverUnitTestsPosh.Jira.CSharp570
+{
+    [TestFixture]
+    public class CSharp570Tests : PoshTestsBase
+    {
+
+
+        [Test]
+        public void Test()
+        {
+            var script = @"
+$cn = [MongoDB.Driver.MongoServer]::Create('mongodb://localhost/');
+$db = $cn['EventLog'];
+[MongoDB.Driver.MongoCollection[System.Diagnostics.EventLogEntry]]$events = $db['Events.Simple'];
+";
+            RunScript(script);
+        }
+    }
+}
