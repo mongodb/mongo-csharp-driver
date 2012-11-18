22cbd961a94f5d3a2e7d7b72022253532b809cc8
Einar Egilsson <einar@einaregilsson.com>
2012-11-18 21:55:46 +0100

    Add .Explain() extension method to IQueryable<T> to explain Linq queries.
    The extension method operates on IQueryable<T> and can therefore not operate
    on .Max, Min, First, Last or any methods that return a scalar. It can also
    not operate on .Distinct queries since they call a specific method on the
    collection and don't give us access to any cursor.

--- MongoDB.Driver/Linq/LinqToMongo.cs
+++ MongoDB.Driver/Linq/LinqToMongo.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright 2010-2013 10gen Inc.
+﻿/* Copyright 2010-2012 10gen Inc.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson;
 
 namespace MongoDB.Driver.Linq
 {
@@ -48,6 +49,7 @@ namespace MongoDB.Driver.Linq
             return source.Any(s => values.Contains(s));
         }
 
+
         /// <summary>
         /// Determines whether a specified value is contained in a sequence.
         /// </summary>
@@ -68,6 +70,39 @@ namespace MongoDB.Driver.Linq
         public static bool Inject(this IMongoQuery query)
         {
             throw new InvalidOperationException("The LinqToMongo.Inject method is only intended to be used in LINQ Where clauses.");
+        }
+
+        /// <summary>
+        /// Returns an explanation of how the query was executed (instead of the results).
+        /// </summary>
+        /// <param name="query">The LINQ query to explain</param>
+        /// <returns>An explanation of thow the query was executed.</returns>
+        public static BsonDocument Explain<T>(this IQueryable<T> query)
+        {
+            return Explain(query, false);
+        }
+
+        /// <summary>
+        /// Returns an explanation of how the query was executed (instead of the results).
+        /// </summary>
+        /// <param name="query">The LINQ query to explain</param>
+        /// <param name="verbose">Whether the explanation should contain more details.</param>
+        /// <returns>An explanation of thow the query was executed.</returns>
+        public static BsonDocument Explain<T>(this IQueryable<T> query, bool verbose)
+        {
+            var selectQuery = (SelectQuery)MongoQueryTranslator.Translate(query);
+            if (selectQuery.Take.HasValue && selectQuery.Take.Value == 0)
+            {
+                throw new NotSupportedException("A query that has a .Take(0) expression will not be sent to the server and can't be explained");
+            }
+            var projector = selectQuery.Execute() as IProjector;
+            if (projector == null)
+            {
+                //This is mainly for .Distinct() queries. First, Last, FirstOrDefault, LastOrDefault don't return
+                //IQueryable<T>, so .Explain() can't be called on them anyway.
+                throw new NotSupportedException("Explain can only be called on Linq queries that return an IProjector");
+            }
+            return projector.Cursor.Explain(verbose);
         }
     }
 }
