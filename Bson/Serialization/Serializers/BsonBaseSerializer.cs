c9546a639c230aa5b09a0771950d8d6ae4994e22
Wesley McClure <wes.mcclure@gmail.com>
2012-11-26 13:50:51 -0600

    My bad, cleanup the code too with this simplification :)

--- Bson/Serialization/Serializers/BsonBaseSerializer.cs
+++ Bson/Serialization/Serializers/BsonBaseSerializer.cs
@@ -48,11 +48,8 @@ namespace MongoDB.Bson.Serialization.Serializers
         /// <param name="defaultSerializationOptions">The default serialization options for this serializer.</param>
         protected BsonBaseSerializer(IBsonSerializationOptions defaultSerializationOptions)
         {
-            if (defaultSerializationOptions != null)
-            {
-            	_defaultSerializationOptions = defaultSerializationOptions;
-            }
-        }
+			_defaultSerializationOptions = defaultSerializationOptions;
+		}
 
         // public properties
         /// <summary>
