7663d2017ea02ea3d1d5a0261a05f2065b7e6ef2
rstam <robert@10gen.com>
2012-11-28 22:28:17 -0500

    CSHARP-498: Minor changes during final diff with x2.0 source tree.

--- MongoDB.Bson/Serialization/Serializers/ArraySerializer.cs
+++ MongoDB.Bson/Serialization/Serializers/ArraySerializer.cs
@@ -14,11 +14,10 @@
 */
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.IO;
 
 using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
