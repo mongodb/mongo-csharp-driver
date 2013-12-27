767e98ef293a9445854108ccaae020f5c966ea90
vorou <vorou@skbkontur.ru>
2013-12-28 00:59:46 +0600

    Parametrize downcase test with multiple lengths

--- MongoDB.BsonUnitTests/Serialization/Conventions/CamelCaseElementNameConventionsTests.cs
+++ MongoDB.BsonUnitTests/Serialization/Conventions/CamelCaseElementNameConventionsTests.cs
@@ -28,7 +28,7 @@ namespace MongoDB.BsonUnitTests.Serialization.Conventions
             public int Age { get; set; }
             public string _dumbName { get; set; }
             public string lowerCase { get; set; }
-            public string IOStream { get; set; }
+            public string ALLCAPS { get; set; }
         }
 
         [Test]
@@ -53,16 +53,18 @@ namespace MongoDB.BsonUnitTests.Serialization.Conventions
         }
 
         [Test]
-        public void TestShouldDowncaseCertainNumberOfChars()
+        [TestCase(2,"alLCAPS")]
+        [TestCase(3,"allCAPS")]
+        [TestCase(4,"allcAPS")]
+        public void TestShouldDowncaseCertainNumberOfChars(int charsToDowncase, string expected)
         {
-            var charsToDowncase = 2;
             var convention = new CamelCaseElementNameConvention(charsToDowncase);
             var classMap = new BsonClassMap<TestClass>();
-            var ioStream = classMap.MapMember(x => x.IOStream);
+            var ioStream = classMap.MapMember(x => x.ALLCAPS);
 
             convention.Apply(ioStream);
 
-            Assert.AreEqual("ioStream", ioStream.ElementName);
+            Assert.AreEqual(expected, ioStream.ElementName);
         }
     }
 }
