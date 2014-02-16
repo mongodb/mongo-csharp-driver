d860a5a3564574ab9432c1e3f85b50bf20395932
ddebilt <dan.debilt@gmail.com>
2014-02-16 15:53:36 -0600

    CSHARP-900: Support downcasting object in LINQ queries

--- MongoDB.DriverUnitTests/Jira/CSharp900Tests.cs
+++ MongoDB.DriverUnitTests/Jira/CSharp900Tests.cs
@@ -22,7 +22,7 @@ using MongoDB.Driver.Builders;
 using MongoDB.Driver.Linq;
 using NUnit.Framework;
 
-namespace MongoDB.DriverUnitTests.Linq
+namespace MongoDB.DriverUnitTests.Jira
 {
     [TestFixture]
     public class CSharp900Tests
