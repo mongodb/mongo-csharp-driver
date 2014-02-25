bc4f0d803c5169298669758fca900f05cc84abf5
Wesley McClure <wes.mcclure@gmail.com>
2014-02-25 09:34:13 -0800

    Add a DecimalRepresentationConvention to change the default representation of a decimal, mirrored from EnumRepresentationConvention

---
+++ MongoDB.Bson/Serialization/Conventions/DecimalRepresentationConvention.cs
@@ -0,0 +1,45 @@
+﻿namespace MongoDB.Bson.Serialization.Conventions
+{
+    using System;
+    using Options;
+
+    /// <summary>
+    /// A convention that allows you to set the Decimal serialization representation
+    /// </summary>
+    public class DecimalRepresentationConvention : ConventionBase, IMemberMapConvention
+    {
+        // private fields
+        private readonly BsonType _representation;
+
+        // constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DecimalRepresentationConvention" /> class.
+        /// </summary>
+        /// <param name="representation">The serialization representation.</param>
+        public DecimalRepresentationConvention(BsonType representation)
+        {
+            if (!((representation == BsonType.Array) ||
+                  (representation == BsonType.Double) ||
+                  (representation == BsonType.Int32) ||
+                  (representation == BsonType.Int64) ||
+                  (representation == BsonType.String)))
+            {
+                throw new ArgumentException("Decimals can only be represented as Array, Double, Int32, Int64 or String");
+            }
+            _representation = representation;
+        }
+
+        /// <summary>
+        /// Changes the decimal representation if the member is a decimal
+        /// </summary>
+        /// <param name="memberMap"></param>
+        public void Apply(BsonMemberMap memberMap)
+        {
+            if (memberMap.MemberType != typeof (decimal))
+            {
+                return;
+            }
+            memberMap.SetSerializationOptions(new RepresentationSerializationOptions(_representation));
+        }
+    }
+}
