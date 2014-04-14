84cb77254e001cda0a267b58f760c615505b1205
Craig Wilson <craiggwilson@gmail.com>
2014-04-14 11:29:16 -0500

    CSHARP-616: removed strong naming and removed the keys.

--- src/MongoDB.Driver/Properties/AssemblyInfo.cs
+++ src/MongoDB.Driver/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("MongoDB.DriverUnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ff4a4832a2b9422e89a1772a78dae7d697e4368c56e609dd0d1281cdc57c1f2de2318cbaa95fcd7c2332825f95ad4bee538ac8125e41105f6cf402363b7e95fed5f357ba0411acab378454f48f8ee87d66bffba859106d084ea65b0b886f2f9518195a9188fda814a61eeea47df63c4b3094cc5780a27abca4414543225258a7")]
+[assembly: InternalsVisibleTo("MongoDB.DriverUnitTests")]
