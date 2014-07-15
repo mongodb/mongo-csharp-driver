1c87217df64a52b1c0ce4f48caf3d6653f16f89b
Craig Wilson <craiggwilson@gmail.com>
2014-07-15 12:25:41 -0500

    added AsyncQueue tests.

--- src/MongoDB.Driver.Core.Tests/Async/InterruptibleDelayTests.cs
+++ src/MongoDB.Driver.Core.Tests/Async/InterruptibleDelayTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Copyright 2013-2014 MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
 using FluentAssertions;
-using NUnit.Framework;
 using MongoDB.Driver.Core.Async;
-using System.Threading;
+using NUnit.Framework;
 
 namespace MongoDB.Driver.Core.Tests.Async
 {
