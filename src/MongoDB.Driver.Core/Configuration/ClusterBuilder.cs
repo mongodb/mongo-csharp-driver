03c5e17037158d43be3b142ee978db37a8f7def5
Craig Wilson <craiggwilson@gmail.com>
2014-07-17 10:14:31 -0500

    added ClusterBuilder.

---
+++ src/MongoDB.Driver.Core/Configuration/ClusterBuilder.cs
@@ -0,0 +1,122 @@
+﻿/* Copyright 2010-2013 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using MongoDB.Driver.Core.Clusters;
+using MongoDB.Driver.Core.ConnectionPools;
+using MongoDB.Driver.Core.Connections;
+using MongoDB.Driver.Core.Misc;
+using MongoDB.Driver.Core.Servers;
+
+namespace MongoDB.Driver.Core.Configuration
+{
+    public class ClusterBuilder
+    {
+        // fields
+        private ClusterSettings _clusterSettings;
+        private ServerSettings _serverSettings;
+        private ConnectionPoolSettings _connectionPoolSettings;
+        private ConnectionSettings _connectionSettings;
+        private TcpStreamSettings _tcpStreamSettings;
+        private Func<IStreamFactory, IStreamFactory> _streamFactoryWrapper;
+
+        // constructors
+        public ClusterBuilder()
+        {
+            _clusterSettings = new ClusterSettings();
+            _serverSettings = new ServerSettings();
+            _connectionPoolSettings = new ConnectionPoolSettings();
+            _connectionSettings = new ConnectionSettings();
+            _tcpStreamSettings = new TcpStreamSettings();
+            _streamFactoryWrapper = inner => inner;
+        }
+
+        // methods
+        public ICluster BuildCluster()
+        {
+            IStreamFactory streamFactory = new TcpStreamFactory(_tcpStreamSettings);
+            // TODO: SSL gets handled here specifically...
+
+            streamFactory = _streamFactoryWrapper(streamFactory);
+
+            var connectionFactory = new BinaryConnectionFactory(
+                _connectionSettings,
+                streamFactory,
+                null);
+
+            var connectionPoolFactory = new ConnectionPoolFactory(
+                connectionFactory,
+                _connectionPoolSettings);
+
+            var serverFactory = new ServerFactory(
+                _serverSettings,
+                connectionPoolFactory,
+                null);
+
+            var clusterFactory = new ClusterFactory(
+                _clusterSettings,
+                serverFactory,
+                null);
+
+            return clusterFactory.Create();
+        }
+
+        public ClusterBuilder ConfigureCluster(Func<ClusterSettings, ClusterSettings> configure)
+        {
+            Ensure.IsNotNull(configure, "configure");
+
+            _clusterSettings = configure(_clusterSettings);
+            return this;
+        }
+
+        public ClusterBuilder ConfigureServer(Func<ServerSettings, ServerSettings> configure)
+        {
+            _serverSettings = configure(_serverSettings);
+            return this;
+        }
+
+        public ClusterBuilder ConfigureConnectionPool(Func<ConnectionPoolSettings, ConnectionPoolSettings> configure)
+        {
+            Ensure.IsNotNull(configure, "configure");
+
+            _connectionPoolSettings = configure(_connectionPoolSettings);
+            return this;
+        }
+
+        public ClusterBuilder ConfigureConnection(Func<ConnectionSettings, ConnectionSettings> configure)
+        {
+            Ensure.IsNotNull(configure, "configure");
+
+            _connectionSettings = configure(_connectionSettings);
+            return this;
+        }
+
+        public ClusterBuilder ConfigureTcp(Func<TcpStreamSettings, TcpStreamSettings> configure)
+        {
+            Ensure.IsNotNull(configure, "configure");
+
+            _tcpStreamSettings = configure(_tcpStreamSettings);
+            return this;
+        }
+
+        public ClusterBuilder RegisterStreamFactory(Func<IStreamFactory, IStreamFactory> wrapper)
+        {
+            Ensure.IsNotNull(wrapper, "wrapper");
+
+            _streamFactoryWrapper = inner => wrapper(_streamFactoryWrapper(inner));
+            return this;
+        }
+    }
+}
---
+++ src/MongoDB.Driver.Core/Configuration/ClusterBuilderExtensionMethods.cs
@@ -0,0 +1,100 @@
+﻿/* Copyright 2010-2013 10gen Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using MongoDB.Driver.Core.Clusters;
+using MongoDB.Driver.Core.Misc;
+
+namespace MongoDB.Driver.Core.Configuration
+{
+    public static class ClusterBuilderExtensionMethods
+    {
+        public static ClusterBuilder ConfigureWithConnectionString(this ClusterBuilder configuration, string connectionString)
+        {
+            Ensure.IsNotNull(configuration, "configuration");
+            Ensure.IsNotNullOrEmpty(connectionString, "connectionString");
+
+            var connString = new ConnectionString(connectionString);
+            return ConfigureWithConnectionString(configuration, connString);
+        }
+
+        public static ClusterBuilder ConfigureWithConnectionString(this ClusterBuilder configuration, ConnectionString connectionString)
+        {
+            Ensure.IsNotNull(configuration, "configuration");
+            Ensure.IsNotNull(connectionString, "connectionString");
+
+            // TCP
+            if(connectionString.ConnectTimeout != null)
+            {
+                // TODO: nowhere to set this
+            }
+
+            if(connectionString.SocketTimeout != null)
+            {
+                configuration.ConfigureTcp(s => s
+                    .WithReadTimeout(connectionString.SocketTimeout.Value)
+                    .WithWriteTimeout(connectionString.SocketTimeout.Value));
+            }
+
+            if(connectionString.Ssl != null)
+            {
+                // TODO: nowhere to set this
+            }
+
+            // Connection
+            if(connectionString.Username != null)
+            {
+                // TODO: nowhere to set this...
+            }
+
+            // Connection Pool
+            if(connectionString.MaxPoolSize != null)
+            {
+                configuration.ConfigureConnectionPool(s => s.WithMaxConnections(connectionString.MaxPoolSize.Value));
+            }
+            if(connectionString.MinPoolSize != null)
+            {
+                // TODO: nowhere to set this
+            }
+            if(connectionString.MaxIdleTime != null)
+            {
+                configuration.ConfigureConnectionPool(s => s.WithConnectionMaxIdleTime(connectionString.MaxIdleTime.Value));
+            }
+            if(connectionString.MaxLifeTime != null)
+            {
+                configuration.ConfigureConnectionPool(s => s.WithConnectionMaxLifeTime(connectionString.MaxLifeTime.Value));
+            }
+            if(connectionString.WaitQueueMultiple != null)
+            {
+                // TODO: nowhere to set this
+            }
+
+            // Server
+            // nothing to configure for server
+
+            //Cluster
+            if(connectionString.Hosts.Count > 0)
+            {
+                configuration.ConfigureCluster(s => s.WithEndPoints(connectionString.Hosts));
+            }
+            if(connectionString.ReplicaSet != null)
+            {
+                configuration.ConfigureCluster(s => s.WithClusterType(ClusterType.ReplicaSet));
+                // TODO: nowhere to set the name
+            }
+
+            return configuration;
+        }
+    }
+}
