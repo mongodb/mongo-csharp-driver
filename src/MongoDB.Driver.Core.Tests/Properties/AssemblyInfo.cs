7fcd1183f89dee3864a7f9f999538f822008e63e
Craig Wilson <craiggwilson@gmail.com>
2014-08-20 14:58:36 -0500

    fixed find-replace mistake with assembly info files.

--- src/MongoDB.Driver.Core.Tests/Properties/AssemblyInfo.cs
+++ src/MongoDB.Driver.Core.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MongoDB.Driver.Core")]
+[assembly: AssemblyTitle("MongoDB.Driver.Core.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MongoDB.Driver.Core")]
+[assembly: AssemblyProduct("MongoDB.Driver.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
