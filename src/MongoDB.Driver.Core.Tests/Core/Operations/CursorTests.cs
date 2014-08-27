193ab85b30d39ecc5b71a1c3f3a7a0c8c8966b6e
rstam <robert@10gen.com>
2014-08-27 11:54:41 -0400

    Added a test that the Cursor constructor calls Dispose on the connectionSource immediately if the cursorId is zero.

---
+++ src/MongoDB.Driver.Core.Tests/Core/Operations/CursorTests.cs
@@ -0,0 +1,57 @@
+﻿/* Copyright 2013-2014 MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Core.Bindings;
+using MongoDB.Driver.Core.Operations;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MongoDB.Driver.Core.Operations
+{
+    [TestFixture]
+    public class CursorTests
+    {
+        [TestCase(0, true)]
+        [TestCase(1, false)]
+        public void Constructor_should_call_Dispose_on_connectionSource_if_cursorId_is_zero(int cursorId, bool shouldCallDispose)
+        {
+            var connectionSource = Substitute.For<IConnectionSource>();
+            var subject = new Cursor<BsonDocument>(
+                connectionSource,
+                "databaseName",
+                "collectionName",
+                new BsonDocument(), // query
+                new BsonDocument[0], // firstBatch
+                cursorId,
+                0, // batchSize
+                0, // limit
+                BsonDocumentSerializer.Instance,
+                null, // messageEncoderSettings
+                Timeout.InfiniteTimeSpan,
+                CancellationToken.None);
+
+            connectionSource.Received(shouldCallDispose ? 1 : 0).Dispose();
+        }
+    }
+}
