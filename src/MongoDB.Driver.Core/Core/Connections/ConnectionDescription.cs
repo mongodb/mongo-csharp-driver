9061b19a2fcf42b18d3659bbb6270682da49a482
Craig Wilson <craiggwilson@gmail.com>
2014-10-30 14:17:22 -0500

    fixing bug introduced in refactoring how server connection ids are handled.

--- src/MongoDB.Driver.Core/Core/Connections/ConnectionDescription.cs
+++ src/MongoDB.Driver.Core/Core/Connections/ConnectionDescription.cs
@@ -114,7 +114,7 @@ namespace MongoDB.Driver.Core.Connections
 
         public ConnectionDescription WithConnectionId(ConnectionId value)
         {
-            return _connectionId.Equals(value) ? this : new ConnectionDescription(value, _isMasterResult, _buildInfoResult);
+            return _connectionId.Equals(value) && _connectionId.ServerValue == value.ServerValue ? this : new ConnectionDescription(value, _isMasterResult, _buildInfoResult);
         }
     }
 }
