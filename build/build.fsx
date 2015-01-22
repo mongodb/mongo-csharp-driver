83ccc1b62bf5c1a34f156d13bed569fddbcc24c8
Craig Wilson <craiggwilson@gmail.com>
2015-01-22 13:08:09 -0600

    fix build number generator.

--- build/build.fsx
+++ build/build.fsx
@@ -7,7 +7,7 @@ let config = getBuildParamOrDefault "config" "Release"
 let baseVersion = getBuildParamOrDefault "baseVersion" "2.0.0"
 let preRelease = getBuildParamOrDefault "preRelease" "local"
 let getComputedBuildNumber() = 
-    let result = Git.CommandHelper.runSimpleGitCommand currentDirectory "describe HEAD^1 --tags --long --match \"v[0-9].[0-9].[0-9]*\""
+    let result = Git.CommandHelper.runSimpleGitCommand currentDirectory "describe HEAD^1 --tags --long --match \"v[0-9].[0-9].[0-9]\""
     let m = System.Text.RegularExpressions.Regex.Match(result, @"-(\d+)-")
     m.Groups.[1].Value
 
