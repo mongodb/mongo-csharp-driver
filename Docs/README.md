e5e3a7ef1c97ce4ced797d7e25bede07eb3ac7ac
Craig Wilson <craiggwilson@gmail.com>
2015-03-26 07:51:25 -0500

    themes and structure.

---
+++ Docs/README.md
@@ -0,0 +1,4 @@
+# MongoDB .NET Driver Documentation
+
+ 1. landing - the front page of all the java docs
+ 2. reference - the reference site for the current version of the docs
---
+++ Docs/reference/themes/mongodb/static/js/scripts.js
@@ -0,0 +1,19 @@
+function initializeJS() {
+    jQuery('.toggle-nav').click(function () {
+        if (jQuery('#sidebar > .ssidebar').is(":visible") === true) {
+            jQuery('#sidebar > .ssidebar').hide();
+            jQuery('#sidebar > .nav-footer').hide();
+            jQuery("#container").addClass("sidebar-closed");
+        } else {
+            jQuery("#container").removeClass("sidebar-closed");
+            jQuery('#sidebar > .ssidebar').show();
+            jQuery('#sidebar > .nav-footer').show();
+        }
+    });
+};
+
+jQuery(document).ready(function(){
+    initializeJS();
+    jQuery("body").addClass("hljsCode");
+    hljs.initHighlightingOnLoad();
+});
