f0db06bd89c1f6a22d97de8eb7ea4efb2b9ef056
rstam <robert@robertstam.org>
2015-04-06 12:53:07 -0400

    CSHARP-1139: Use NuGet to manage our dependency on NUnit.

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-mono --runtime=v4.0 Tools/NuGet/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-
-mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO ./build/build.fsx
+mono --runtime=v4.0 Tools/NuGet/NuGet.exe install FAKE -OutputDirectory Tools -ExcludeVersion
+mono --runtime=v4.0 Tools/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO ./build/build.fsx
