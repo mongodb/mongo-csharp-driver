9c5fa89f7f01ee9295fb42a53e1d408d523c8f10
tugberkugurlu <tugberkugurlu@gmail.com>
2015-05-06 21:47:50 +0100

    added drop extension methods for IMongoDatabase and IMongoCollection

---
+++ src/MongoDB.Driver/MongoDatabaseExtensions.cs
@@ -0,0 +1,39 @@
+﻿/* Copyright 2010-2014 MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MongoDB.Driver
+{
+    /// <summary>
+    /// Holds the extensions for <see cref="IMongoDatabase" />.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class MongoDatabaseExtensions
+    {
+        /// <summary>
+        /// Drops the referenced database.
+        /// </summary>
+        /// <param name="database">The database reference to drop.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>A task</returns>
+        public static Task DropAsync(this IMongoDatabase database, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return database.Client.DropDatabaseAsync(database.DatabaseNamespace.DatabaseName, cancellationToken);
+        }
+    }
+}
