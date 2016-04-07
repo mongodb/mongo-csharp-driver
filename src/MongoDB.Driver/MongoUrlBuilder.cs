3e322cbcc21df93302af41fd830b63bf8532a6f4
Carl Reinke <mindless2112@users.noreply.github.com>
2016-04-07 12:24:27 -0600

    Remove reference to removed class in documentation.

--- src/MongoDB.Driver/MongoUrlBuilder.cs
+++ src/MongoDB.Driver/MongoUrlBuilder.cs
@@ -30,8 +30,7 @@ using MongoDB.Shared;
 namespace MongoDB.Driver
 {
     /// <summary>
-    /// Represents URL style connection strings. This is the recommended connection string style, but see also
-    /// MongoConnectionStringBuilder if you wish to use .NET style connection strings.
+    /// Represents URL-style connection strings.
     /// </summary>
 #if NET45
     [Serializable]
