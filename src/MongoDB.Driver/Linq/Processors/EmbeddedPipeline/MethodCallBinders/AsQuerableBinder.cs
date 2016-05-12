a2d2794b3d864587cc64efeb0e5c08e5994ca9de
Alexandar Aramov <alex.aramov@gmail.com>
2016-05-12 16:09:45 +0300

    Adding AsQuerable Binder

---
+++ src/MongoDB.Driver/Linq/Processors/EmbeddedPipeline/MethodCallBinders/AsQuerableBinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using MongoDB.Driver.Linq.Expressions;
+
+namespace MongoDB.Driver.Linq.Processors.EmbeddedPipeline.MethodCallBinders
+{
+    using System.Linq;
+    using System.Reflection;
+
+    internal class AsQuerableBinder : IMethodCallBinder<EmbeddedPipelineBindingContext>
+    {
+        public static IEnumerable<MethodInfo> GetSupportedMethods()
+        {
+            yield return MethodHelper.GetMethodDefinition(() => Queryable.AsQueryable(null));
+            yield return MethodHelper.GetMethodDefinition(() => Queryable.AsQueryable<object>(null));
+        }
+
+        public Expression Bind(
+            PipelineExpression pipeline,
+            EmbeddedPipelineBindingContext bindingContext,
+            MethodCallExpression node,
+            IEnumerable<Expression> arguments)
+        {
+            return pipeline;
+        }
+    }
+}
