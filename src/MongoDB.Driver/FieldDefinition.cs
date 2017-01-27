9640b12c16396517a6c928733b11eef4bf6295e1
rstam <robert@robertstam.org>
2017-01-27 11:01:57 -0500

    CSHARP-1891: Code review changes.

--- src/MongoDB.Driver/FieldDefinition.cs
+++ src/MongoDB.Driver/FieldDefinition.cs
@@ -78,6 +78,7 @@ namespace MongoDB.Driver
         /// </summary>
         /// <param name="fieldName">The field name.</param>
         /// <param name="fieldSerializer">The field serializer.</param>
+        [Obsolete("Use the constructor that takes 4 arguments instead.")]
         public RenderedFieldDefinition(string fieldName, IBsonSerializer<TField> fieldSerializer)
             : this(fieldName, fieldSerializer, fieldSerializer, fieldSerializer)
         {
