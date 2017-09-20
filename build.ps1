519ddd925084795e2903060848eab10c6d376425
rstam <robert@robertstam.org>
2017-09-20 11:07:32 -0400

    Upgrade build.ps1 because latest version of CAKE broke the build.

--- build.ps1
+++ build.ps1
@@ -16,7 +16,7 @@ No tasks will be executed.
 .PARAMETER ScriptArgs
 Remaining arguments are added here.
 .LINK
-http://cakebuild.net
+https://cakebuild.net
 #>
 
 [CmdletBinding()]
@@ -31,11 +31,15 @@ Param(
     [string[]]$ScriptArgs
 )
 
-$CakeVersion = "0.19.5"
+$CakeVersion = "0.22.2"
 $DotNetChannel = "preview";
-$DotNetVersion = "1.0.0-preview2-003121";
-$DotNetInstallerUri = "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0-preview2/scripts/obtain/dotnet-install.ps1";
+$DotNetVersion = "1.0.4";
+$DotNetInstallerUri = "https://dot.net/v1/dotnet-install.ps1";
 $NugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
+# Temporarily skip verification and opt-in to new in-proc NuGet
+$ENV:CAKE_SETTINGS_SKIPVERIFICATION='true'
+$ENV:CAKE_NUGET_USEINPROCESSCLIENT='true'
 
 # Make sure tools folder exists
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
@@ -82,9 +86,10 @@ if($FoundDotNetCliVersion -ne $DotNetVersion) {
 
     Remove-PathVariable "$InstallPath"
     $env:PATH = "$InstallPath;$env:PATH"
-    $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-    $env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 }
+
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+$env:DOTNET_CLI_TELEMETRY_OPTOUT=1
 
 ###########################################################################
 # INSTALL NUGET
