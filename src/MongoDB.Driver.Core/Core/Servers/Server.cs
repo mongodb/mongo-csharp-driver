685a6d812f7af09c4e72fff390f842c122c4ff50
rstam <robert@robertstam.org>
2018-05-03 19:51:04 -0400

    CSHARP-2259: When a MongoNodeIsRecoveringException occurs the server type must be set to Unknown.

--- src/MongoDB.Driver.Core/Core/Servers/Server.cs
+++ src/MongoDB.Driver.Core/Core/Servers/Server.cs
@@ -47,6 +47,7 @@ namespace MongoDB.Driver.Core.Servers
         private static readonly List<Type> __invalidatingExceptions = new List<Type>
         {
             typeof(MongoNotPrimaryException),
+            typeof(MongoNodeIsRecoveringException),
             typeof(MongoConnectionException),
             typeof(SocketException),
             typeof(EndOfStreamException),
