a4f0b08ebd5bae7c9819c890b9781888ccbcaa96
Wan Bachtiar <sindbach@gmail.com>
2018-07-14 07:12:24 +1000

    Updated to conform with other changes

--- tests/MongoDB.Driver.Examples/TransactionExamplesForDocs/IntroExample1.cs
+++ tests/MongoDB.Driver.Examples/TransactionExamplesForDocs/IntroExample1.cs
@@ -24,23 +24,8 @@ namespace MongoDB.Driver.Examples.TransactionExamplesForDocs
         public void UpdateEmployeeInfo(IMongoClient client, IClientSessionHandle session)
         {
 
-            var hrDatabase = client.GetDatabase("hr");
-            var reportingDatabase = client.GetDatabase("reporting");
-
-            try
-            {
-                // Ensure collection namespaces exist before starting transaction.
-                hrDatabase.CreateCollection("employees");
-                reportingDatabase.CreateCollection("events");
-            }
-            catch (MongoCommandException exception)
-            {
-                // Ignore if collections exist.
-                Console.WriteLine($"Caught exception while creating collection: {exception.Message}.");
-            }
-
-            var employeesCollection = hrDatabase.GetCollection<BsonDocument>("employees");
-            var eventsCollection = reportingDatabase.GetCollection<BsonDocument>("events");
+            var employeesCollection = client.GetDatabase("hr").GetCollection<BsonDocument>("employees");
+            var eventsCollection = client.GetDatabase("reporting").GetCollection<BsonDocument>("events");
 
             session.StartTransaction(new TransactionOptions(
                 readConcern: ReadConcern.Snapshot,
