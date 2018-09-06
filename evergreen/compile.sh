d4089771c6a9a7cdd108d0ab3c7ef1a4bbeb9316
Mark Benvenuto <mark.benvenuto@mongodb.com>
2018-09-06 19:35:32 -0400

    CSHARP-2379 Make evergreen compile use diagnostic output for cake compile

--- evergreen/compile.sh
+++ evergreen/compile.sh
@@ -10,4 +10,4 @@ set -o errexit  # Exit the script with error if any of the commands fail
 echo "Compiling .NET driver"
 
 for var in TMP TEMP NUGET_PACKAGES NUGET_HTTP_CACHE_PATH APPDATA; do setx $var z:\\data\\tmp; export $var=z:\\data\\tmp; done
-powershell.exe .\\build.ps1 -target Build
+powershell.exe .\\build.ps1 -target Build -Verbosity Diagnostic
