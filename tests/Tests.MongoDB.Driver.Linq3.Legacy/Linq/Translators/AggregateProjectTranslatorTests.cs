16ce73ea65f032530928569cab4d5071b7d8cc1f
rstam <robert@robertstam.org>
2020-09-25 16:23:04 -0700

    Worked on All and Any translators.

--- tests/Tests.MongoDB.Driver.Linq3.Legacy/Linq/Translators/AggregateProjectTranslatorTests.cs
+++ tests/Tests.MongoDB.Driver.Linq3.Legacy/Linq/Translators/AggregateProjectTranslatorTests.cs
@@ -95,7 +95,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy.Translators
 
             var result = Project(x => new { Result = x.G.All(g => g.E.F > 30) });
 
-            result.Projection.Should().Be("{ Result: { \"$allElementsTrue\" : { \"$map\": { input: \"$G\", as: \"g\", in: { \"$gt\": [\"$$g.E.F\", 30 ] } } } }, _id: 0 }");
+            result.Projection.Should().Be("{ Result: { $reduce : { input : '$G', initialValue : true, in : { $cond : { if : '$$value', then : { $gt : ['$$this.E.F', 30] }, else : false } } } }, _id : 0 }");
 
             result.Value.Result.Should().BeTrue();
         }
@@ -119,7 +119,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy.Translators
 
             var result = Project(x => new { Result = x.G.Any(g => g.E.F > 40) });
 
-            result.Projection.Should().Be("{ Result: { \"$anyElementTrue\" : { \"$map\": { input: \"$G\", as: \"g\", in: { \"$gt\": [\"$$g.E.F\", 40 ] } } } }, _id: 0 }");
+            result.Projection.Should().Be("{ Result : { $reduce : { input : '$G', initialValue : false, in : { $cond : { if : '$$value', then : true, else : { $gt : ['$$this.E.F', 40] } } } } }, _id : 0 }");
 
             result.Value.Result.Should().BeTrue();
         }
@@ -600,7 +600,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy.Translators
         }
 
         [SkippableFact]
-        public void Should_translate_indexOfBytes()
+        public void Should_translate_indexOf()
         {
             RequireServer.Check().VersionGreaterThanOrEqualTo("3.3.6");
 
