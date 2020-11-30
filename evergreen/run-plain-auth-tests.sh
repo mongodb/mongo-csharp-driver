b3411f1cc3928a9308329b08228e9b95bed4f4f4
Durran Jordan <durran@gmail.com>
2020-11-30 19:18:08 +0100

    Revert to original plain auth test script

---
+++ evergreen/run-plain-auth-tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Don't trace since the URI contains a password that shouldn't show up in the logs
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       MONGODB_URI             Set the URI, including username/password to use to connect to the server via PLAIN authentication mechanism
+
+############################################
+#            Main Program                  #
+############################################
+
+echo "Running PLAIN authentication tests"
