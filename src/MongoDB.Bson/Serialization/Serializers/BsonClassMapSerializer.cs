f1c3b1b0903ebdbbbaa465ec0913767d4668364e
Boris <boris.dogadov@mongodb.com>
2020-12-04 10:58:02 -0800

    - Removed comment referencing the removed variable

--- src/MongoDB.Bson/Serialization/Serializers/BsonClassMapSerializer.cs
+++ src/MongoDB.Bson/Serialization/Serializers/BsonClassMapSerializer.cs
@@ -624,13 +624,6 @@ namespace MongoDB.Bson.Serialization
                 SerializeMember(context, document, idMemberMap);
             }
 
-            //var autoTimeStampMemberMap = _classMap.AutoTimeStampMemberMap;
-            //if (autoTimeStampMemberMap != null)
-            //{
-            //    SerializeNormalMember(context, document, autoTimeStampMemberMap);
-            //    remainingMemberMaps.Remove(autoTimeStampMemberMap);
-            //}
-
             if (ShouldSerializeDiscriminator(args.NominalType))
             {
                 SerializeDiscriminator(context, args.NominalType, document);
