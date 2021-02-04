acd02d5847c30848677371f920624b6a2ca2e131
rstam <robert@robertstam.org>
2021-02-04 08:20:09 -0800

    CSHARP-3381: LINQ3: Get MongoQueryableIntComparedToNullableIntWithStringRepresentationTests to pass

--- tests/Tests.MongoDB.Driver.Linq3.Legacy/Linq/MongoQueryableIntComparedToNullableIntWithStringRepresentationTests.cs
+++ tests/Tests.MongoDB.Driver.Linq3.Legacy/Linq/MongoQueryableIntComparedToNullableIntWithStringRepresentationTests.cs
@@ -19,6 +19,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq3;
+using MongoDB.Driver.Linq3.Translators.ExpressionToExecutableQueryTranslators;
 using MongoDB.Driver.Tests;
 using Xunit;
 
@@ -52,7 +53,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I == value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
         }
 
         [Theory]
@@ -64,7 +65,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I > value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
         }
 
         [Theory]
@@ -76,7 +77,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I >= value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
         }
 
         [Theory]
@@ -88,7 +89,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I < value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
         }
 
         [Theory]
@@ -100,7 +101,7 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I <= value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
         }
 
         [Theory]
@@ -112,7 +113,22 @@ namespace Tests.MongoDB.Driver.Linq3.Legacy
 
             var queryable = subject.Where(x => x.I != value);
 
-            queryable.ToString().Should().Be($"aggregate([{{ \"$match\" : {expectedFilter} }}])");
+            AssertFilter(queryable, expectedFilter);
+        }
+
+        // private methods
+        private void AssertFilter<T>(IQueryable<T> queryable, string expectedFilter)
+        {
+            var stages = Translate(queryable);
+            stages.Should().HaveCount(1);
+            stages[0].Should().Be($"{{ \"$match\" : {expectedFilter} }}");
+        }
+
+        private BsonDocument[] Translate<T>(IQueryable<T> queryable)
+        {
+            var provider = (MongoQueryProvider<T>)queryable.Provider;
+            var executableQuery = ExpressionToExecutableQueryTranslator.Translate<T, T>(provider, queryable.Expression);
+            return executableQuery.Stages;
         }
     }
 }
