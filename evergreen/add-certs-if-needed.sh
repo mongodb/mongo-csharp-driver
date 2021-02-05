1123d538f10f99561391f1d7a8865ed2d30a6392
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-02-05 23:11:01 +0300

    CSHARP-3299: Enable Secure Tests on Linux.

--- evergreen/add-certs-if-needed.sh
+++ evergreen/add-certs-if-needed.sh
@@ -7,6 +7,7 @@ set -o errexit  # Exit the script with an error if any of the commands fail
 #     SSL                     Set to enable SSL. Values are "ssl" / "nossl" (default)
 #     OCSP_TLS_SHOULD_SUCCEED Set to test OCSP. Values are true/false/nil
 #     OCSP_ALGORITHM          Set to test OCSP. Values are rsa/ecdsa/nil
+#     OS                      Set to access operating system
 
 SSL=${SSL:-nossl}
 OCSP_TLS_SHOULD_SUCCEED=${OCSP_TLS_SHOULD_SUCCEED:-nil}
@@ -16,16 +17,26 @@ if [[ "$SSL" != "ssl" ]]; then
   exit 0
 fi
 
-if [[ "$OS" =~ Windows|windows ]]; then
-  certutil.exe -addstore "Root" ${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem
-
-  if [[ "$OCSP_TLS_SHOULD_SUCCEED" != "nil" && "$OCSP_ALGORITHM" != "nil" ]]; then
-    certutil.exe -addstore "Root" ${DRIVERS_TOOLS}/.evergreen/ocsp/${OCSP_ALGORITHM}/ca.pem
+function make_trusted() {
+  echo "CA.pem certificate $1"
+  if [[ "$OS" =~ Windows|windows ]]; then
+    # makes the client.pem trusted
+    certutil.exe -addstore "Root" $1
+  elif  [[ "$OS" =~ Ubuntu|ubuntu ]]; then
+    # makes the client.pem trusted
+    # note: .crt is the equivalent format as .pem, but we need to make this renaming because update-ca-certificates supports only .crt
+    sudo cp -f $1 /usr/local/share/ca-certificates/ca.crt
+    sudo update-ca-certificates
+  else
+    # mac OS, the same trick as for above ubuntu step
+    sudo cp -f $1 ~/ca.crt
+    sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ~/ca.crt
+    #sudo security delete-certificate -c "<name of existing certificate>"
   fi
-else
-  keytool -import -trustcacerts -file ${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem -keystore Root -storepass changeit -noprompt -alias x509
+}
 
-  if [[ "$OCSP_TLS_SHOULD_SUCCEED" != "nil" && "$OCSP_ALGORITHM" != "nil" ]]; then
-    keytool -import -trustcacerts -file ${DRIVERS_TOOLS}/.evergreen/ocsp/${OCSP_ALGORITHM}/ca.pem -keystore Root -storepass changeit -noprompt -alias ${OCSP_ALGORITHM}
-  fi
+make_trusted ${DRIVERS_TOOLS}/.evergreen/x509gen/ca.pem
+
+if [[ "$OCSP_TLS_SHOULD_SUCCEED" != "nil" && "$OCSP_ALGORITHM" != "nil" ]]; then
+  make_trusted ${DRIVERS_TOOLS}/.evergreen/ocsp/${OCSP_ALGORITHM}/ca.pem
 fi
