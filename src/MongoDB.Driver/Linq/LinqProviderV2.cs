fe17be2cc17d69f73239b1ed20e27532bae2d1b9
rstam <robert@robertstam.org>
2021-04-23 11:26:29 -0700

    CSHARP-3591: LINQ3: Add switch to MongoClientSettings for applications to opt-in to LINQ3

---
+++ src/MongoDB.Driver/Linq/LinqProviderV2.cs
@@ -0,0 +1,30 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System.Threading;
+
+namespace MongoDB.Driver.Linq
+{
+    internal sealed class LinqProviderV2 : LinqProvider
+    {
+        internal override IMongoQueryable<TDocument> AsQueryable<TDocument>(IMongoCollection<TDocument> collection, IClientSessionHandle session, AggregateOptions options, CancellationToken cancellationToken)
+        {
+            var queryProvider = new Linq2.MongoQueryProviderImpl<TDocument>(collection, session, options);
+            return new Linq2.MongoQueryableImpl<TDocument, TDocument>(queryProvider);
+        }
+
+        public override string ToString() => "V2";
+    }
+}
