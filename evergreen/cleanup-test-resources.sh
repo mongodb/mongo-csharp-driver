fc1c842f5db7d16fc675deb7b3fcafa95f690260
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-08-26 20:05:27 +0300

    CSHARP-3809: Mov logic into a separate file.

---
+++ evergreen/cleanup-test-resources.sh
@@ -0,0 +1,17 @@
+set -o xtrace   # Write all commands first to stderr
+
+# Environment variables used as input:
+#   OS                                               The current operating system
+
+echo "Attempt to kill mongocryptd process if presented"
+if [ "Windows_NT" = "$OS" ]; then
+  tasklist -FI "IMAGENAME eq mongocryptd.exe"
+  taskkill -F -FI "IMAGENAME eq mongocryptd.exe"
+  # check that it's actually killed
+  tasklist -FI "IMAGENAME eq mongocryptd.exe"
+else
+  ps -ax | grep mongocryptd
+  pkill -f 'mongocryptd' || echo 'mongocryptd was already killed or not launched'
+  # check that it's actually killed
+  ps -ax | grep mongocryptd 
+fi
