a031352068d4c62eed721d8f8c275f8757c530ec
James Kovacs <jkovacs@post.harvard.edu>
2021-11-03 16:44:34 -0600

    CSHARP-3434: Fixed OCSP tests in Evergreen.

--- evergreen/prepare-ocsp.sh
+++ evergreen/prepare-ocsp.sh
@@ -11,6 +11,7 @@ if [ "Windows_NT" = "$OS" ]; then # Magic variable in cygwin
   /cygdrive/c/python/python38/python.exe -m venv ./venv
   ./venv/Scripts/pip3 install -r ${DRIVERS_TOOLS}/.evergreen/ocsp/mock-ocsp-responder-requirements.txt
 else
+  echo "$0 needs to be updated to run on non-Windows platforms"
   # Need to ensure on Linux python is installed in the correct place and visible to the script.
   # https://jira.mongodb.org/browse/CSHARP-3255
   # /opt/python/2.7/bin/python -m venv ./venv
