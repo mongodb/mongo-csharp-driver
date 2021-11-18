22ab2cd8e3f8a7c6f25a73e08ff476c5f96753b9
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2021-11-19 01:34:01 +0300

    CSHARP-3936: Code review.

--- evergreen/cleanup-test-resources.sh
+++ evergreen/cleanup-test-resources.sh
@@ -3,7 +3,7 @@ set -o xtrace   # Write all commands first to stderr
 # Environment variables used as input:
 #   OS                                               The current operating system
 
-echo "Attempt to kill mongocryptd process if presented"
+echo "Attempt to kill mongocryptd process if presented on ${OS}"
 if [ "Windows_NT" = "$OS" ]; then
   tasklist -FI "IMAGENAME eq mongocryptd.exe"
   taskkill -F -FI "IMAGENAME eq mongocryptd.exe"
