464b2955f2030269e63e5804d29c4381ae806de8
James Kovacs <jkovacs@post.harvard.edu>
2021-11-24 11:12:05 -0700

    Added v2.14.0 docs.

--- 2.14/apidocs/scripts/branding-Website.js
+++ 2.14/apidocs/scripts/branding-Website.js
@@ -9,7 +9,7 @@
 // This file contains the methods necessary to implement the lightweight TOC and search functionality.
 //
 // This code is published under the Microsoft Public License (Ms-PL).  A copy of the license should be
-// distributed with the code.  It can also be found at the project website: https://GitHub.com/EWSoftware/SHFB.  This
+// distributed with the code and can be found at the project website: https://GitHub.com/EWSoftware/SHFB.  This
 // notice, the author's name, and all copyright notices must remain intact in all applications, documentation,
 // and source files.
 //
