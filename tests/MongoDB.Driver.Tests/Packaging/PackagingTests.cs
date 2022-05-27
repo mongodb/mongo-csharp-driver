28cad951e8f8b460b21b51e9554f5be57e88ead1
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2022-05-27 23:01:59 +0400

    Merge tests.

--- tests/MongoDB.Driver.Tests/Packaging/PackagingTests.cs
+++ tests/MongoDB.Driver.Tests/Packaging/PackagingTests.cs
@@ -38,7 +38,7 @@ namespace MongoDB.Driver.Tests.Packaging
         {
             var version = Library.Version;
 
-            version.Should().Be("1.5.0-alpha0");
+            version.Should().Be("1.5.0-alpha2");
         }
 
         [Fact]
