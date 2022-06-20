77a8729db2723c89cf24de4e5c4692c62afd91e2
mingyaulee <lee_mingyau@hotmail.com>
2022-06-20 18:32:50 +0800

    CSHARP-4205: Add unit tests for fix

--- tests/MongoDB.Bson.Tests/Serialization/Serializers/GenericEnumerableSerializerTests.cs
+++ tests/MongoDB.Bson.Tests/Serialization/Serializers/GenericEnumerableSerializerTests.cs
@@ -13,6 +13,7 @@
 * limitations under the License.
 */
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using MongoDB.Bson;
@@ -204,6 +205,108 @@ namespace MongoDB.Bson.Tests.Serialization.GenericEnumerable
             var bson = obj.ToBson();
             var rehydrated = BsonSerializer.Deserialize<TestClass>(bson);
             Assert.IsType<List<Address>>(rehydrated.Addresses);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+    }
+
+    public class ICustomCollectionSerializerTests
+    {
+        public interface IAddressCollection : ICollection<Address>
+        {
+        }
+
+        public class AddressCollection : IAddressCollection
+        {
+            private readonly List<Address> data = new();
+            public int Count => data.Count;
+            public bool IsReadOnly => false;
+            public void Add(Address item) => data.Add(item);
+            public void Clear() => data.Clear();
+            public bool Contains(Address item) => data.Contains(item);
+            public void CopyTo(Address[] array, int arrayIndex) => data.CopyTo(array, arrayIndex);
+            public IEnumerator<Address> GetEnumerator() => data.GetEnumerator();
+            public bool Remove(Address item) => data.Remove(item);
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        public class TestClass
+        {
+            public IAddressCollection Addresses { get; set; }
+        }
+
+        [Fact]
+        public void TestNull()
+        {
+            var obj = new TestClass { Addresses = null };
+            var json = obj.ToJson();
+            var expected = "{ 'Addresses' : null }".Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<TestClass>(bson);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Fact]
+        public void TestEmpty()
+        {
+            var obj = new TestClass
+            {
+                Addresses = new AddressCollection()
+            };
+            var json = obj.ToJson();
+            var expected = "{ 'Addresses' : { '_t' : 'AddressCollection', '_v' : [] } }".Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<TestClass>(bson);
+            Assert.IsType<AddressCollection>(rehydrated.Addresses);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Fact]
+        public void TestOneAddress()
+        {
+            var obj = new TestClass
+            {
+                Addresses = new AddressCollection
+                {
+                    new Address { Street = "123 Main", City = "Smithtown", State = "PA", Zip = 12345 }
+                }
+            };
+            var json = obj.ToJson();
+            var expected = "{ 'Addresses' : { '_t' : 'AddressCollection', '_v' : [#A1] } }";
+            expected = expected.Replace("#A1", "{ 'Street' : '123 Main', 'City' : 'Smithtown', 'State' : 'PA', 'Zip' : 12345 }");
+            expected = expected.Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<TestClass>(bson);
+            Assert.IsType<AddressCollection>(rehydrated.Addresses);
+            Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
+        }
+
+        [Fact]
+        public void TestTwoAddresses()
+        {
+            var obj = new TestClass
+            {
+                Addresses = new AddressCollection
+                {
+                    new Address { Street = "123 Main", City = "Smithtown", State = "PA", Zip = 12345 },
+                    new Address { Street = "456 First", City = "Johnstown", State = "MD", Zip = 45678 }
+                }
+            };
+            var json = obj.ToJson();
+            var expected = "{ 'Addresses' : { '_t' : 'AddressCollection', '_v' : [#A1, #A2] } }";
+            expected = expected.Replace("#A1", "{ 'Street' : '123 Main', 'City' : 'Smithtown', 'State' : 'PA', 'Zip' : 12345 }");
+            expected = expected.Replace("#A2", "{ 'Street' : '456 First', 'City' : 'Johnstown', 'State' : 'MD', 'Zip' : 45678 }");
+            expected = expected.Replace("'", "\"");
+            Assert.Equal(expected, json);
+
+            var bson = obj.ToBson();
+            var rehydrated = BsonSerializer.Deserialize<TestClass>(bson);
+            Assert.IsType<AddressCollection>(rehydrated.Addresses);
             Assert.True(bson.SequenceEqual(rehydrated.ToBson()));
         }
     }
