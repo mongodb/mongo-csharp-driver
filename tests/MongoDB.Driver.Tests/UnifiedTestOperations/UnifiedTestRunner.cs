d3383afce2ecd0e13dc7748f3e0f81071d0da6d2
BorisDog <BorisDog@users.noreply.github.com>
2022-10-17 08:48:09 -0700

    CSHARP-4346: Add log messages to Command monitoring spec

--- tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedTestRunner.cs
+++ tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedTestRunner.cs
@@ -99,7 +99,7 @@ namespace MongoDB.Driver.Tests.UnifiedTestOperations
 
             var schemaSemanticVersion = SemanticVersion.Parse(schemaVersion);
             if (schemaSemanticVersion < new SemanticVersion(1, 0, 0) ||
-                schemaSemanticVersion > new SemanticVersion(1, 12, 0))
+                schemaSemanticVersion > new SemanticVersion(1, 13, 0))
             {
                 throw new FormatException($"Schema version '{schemaVersion}' is not supported.");
             }
