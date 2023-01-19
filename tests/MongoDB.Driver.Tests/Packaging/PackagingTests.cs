16e5726471b30f8262ec1c504abd7055343e7895
DmitryLukyanov <dmitry.lukyanov@mongodb.com>
2023-01-20 01:36:33 +0400

    Fixing packaging tests.

--- tests/MongoDB.Driver.Tests/Packaging/PackagingTests.cs
+++ tests/MongoDB.Driver.Tests/Packaging/PackagingTests.cs
@@ -38,7 +38,7 @@ namespace MongoDB.Driver.Tests.Packaging
         {
             var version = Library.Version;
 
-            version.Should().Be("1.7.0-alpha1");
+            version.Should().Be("1.7.0-alpha2");
         }
 
 #pragma warning disable IDE0051 // Remove unused private members
