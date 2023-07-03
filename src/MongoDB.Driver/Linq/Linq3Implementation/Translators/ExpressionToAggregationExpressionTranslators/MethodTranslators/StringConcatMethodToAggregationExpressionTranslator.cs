b1f4bdec856b877824b12ad7b5a46032d70faf7d
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2023-07-03 12:34:29 -0700

    CSHARP-4695: Expression not supported: string.Concat (#1123)

---
+++ src/MongoDB.Driver/Linq/Linq3Implementation/Translators/ExpressionToAggregationExpressionTranslators/MethodTranslators/StringConcatMethodToAggregationExpressionTranslator.cs
@@ -0,0 +1,73 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Linq.Linq3Implementation.Ast.Expressions;
+using MongoDB.Driver.Linq.Linq3Implementation.Misc;
+
+namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToAggregationExpressionTranslators.MethodTranslators
+{
+    internal static class StringConcatMethodToAggregationExpressionTranslator
+    {
+        private static readonly MethodInfo[] __stringConcatMethods = new[]
+        {
+            StringMethod.ConcatWith2Strings,
+            StringMethod.ConcatWith3Strings,
+            StringMethod.ConcatWith4Strings,
+            StringMethod.ConcatWithStringArray
+        };
+
+        public static bool CanTranslate(MethodCallExpression expression)
+            => expression.Method.IsOneOf(__stringConcatMethods);
+
+        public static AggregationExpression Translate(TranslationContext context, MethodCallExpression expression)
+        {
+            var method = expression.Method;
+            var arguments = expression.Arguments;
+
+            IEnumerable<AstExpression> argumentsTranslations = null;
+
+            if (method.IsOneOf(
+                StringMethod.ConcatWith2Strings,
+                StringMethod.ConcatWith3Strings,
+                StringMethod.ConcatWith4Strings))
+            {
+                argumentsTranslations =
+                    arguments.Select(a => ExpressionToAggregationExpressionTranslator.Translate(context, a).Ast);
+            }
+
+            if (method.Is(StringMethod.ConcatWithStringArray))
+            {
+                var argumentTranslation = ExpressionToAggregationExpressionTranslator.Translate(context, arguments.Single());
+                if (argumentTranslation.Ast is AstComputedArrayExpression astArray)
+                {
+                    argumentsTranslations = astArray.Items;
+                }
+            }
+
+            if (argumentsTranslations != null)
+            {
+                var ast = AstExpression.Concat(argumentsTranslations.ToArray());
+                return new AggregationExpression(expression, ast, StringSerializer.Instance);
+            }
+
+            throw new ExpressionNotSupportedException(expression);
+        }
+    }
+}
---
+++ tests/MongoDB.Driver.Tests/Linq/Linq3Implementation/Translators/ExpressionToAggregationExpressionTranslators/MethodTranslators/StringConcatMethodToAggregationExpressionTranslatorTests.cs
@@ -0,0 +1,186 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace MongoDB.Driver.Tests.Linq.Linq3Implementation.Translators.ExpressionToAggregationExpressionTranslators.MethodTranslators
+{
+    public class StringConcatMethodToAggregationExpressionTranslatorTests : Linq3IntegrationTest
+    {
+        [Fact]
+        public void Filter_using_string_concat_with_two_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => string.Concat(i.A, ";") == "A1;");
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { $expr : { $eq : [{ $concat : ['$A', ';'] }, 'A1;'] } } }");
+
+            var result = queryable.Single();
+            result.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void Projection_using_string_concat_with_two_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => i.Id == 1)
+                .Select(i => new { T = string.Concat(i.A, ";") });
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { _id : 1 } }",
+                "{ $project : { T : { $concat : ['$A', ';'] }, _id : 0 } }");
+
+            var result = queryable.Single();
+            result.T.Should().Be("A1;");
+        }
+
+        [Fact]
+        public void Filter_using_string_concat_with_three_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => string.Concat(i.A, ";", i.B) == "A1;B1");
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { $expr : { $eq : [{ $concat : ['$A', ';', '$B'] }, 'A1;B1'] } } }");
+
+            var result = queryable.Single();
+            result.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void Projection_using_string_concat_with_three_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => i.Id == 1)
+                .Select(i => new { T = string.Concat(i.A, ";", i.B) });
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { _id : 1 } }",
+                "{ $project : { T : { $concat : ['$A', ';', '$B'] }, _id : 0 } }");
+
+            var result = queryable.Single();
+            result.T.Should().Be("A1;B1");
+        }
+
+        [Fact]
+        public void Filter_using_string_concat_with_four_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => string.Concat(i.A, ";", i.B, i.C) == "A1;B1C1");
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { $expr : { $eq : [{ $concat : ['$A', ';', '$B', '$C'] }, 'A1;B1C1'] } } }");
+
+            var result = queryable.Single();
+            result.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void Projection_using_string_concat_with_four_strings_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => i.Id == 1)
+                .Select(i => new { T = string.Concat(i.A, ";", i.B, i.C) });
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { _id : 1 } }",
+                "{ $project : { T : { $concat : ['$A', ';', '$B', '$C'] }, _id : 0 } }");
+
+            var result = queryable.Single();
+            result.T.Should().Be("A1;B1C1");
+        }
+
+        [Fact]
+        public void Filter_using_string_concat_with_params_array_should_work()
+        {
+            var collection = CreateCollection();
+            var queryable = collection.AsQueryable()
+                .Where(i => string.Concat(i.A, ";", i.B, ";", i.C) == "A1;B1;C1");
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { $expr : { $eq : [{ $concat : ['$A', ';', '$B', ';', '$C'] }, 'A1;B1;C1'] } } }");
+
+            var result = queryable.Single();
+            result.Id.Should().Be(1);
+        }
+
+        [Fact]
+        public void Projection_using_string_concat_with_params_array_should_work()
+        {
+            var collection = CreateCollection();
+
+            var queryable = collection.AsQueryable()
+                .Where(i => i.Id == 1)
+                .Select(i => new { T = string.Concat(i.A, ";", i.B, ";", i.C) });
+
+            var stages = Translate(collection, queryable);
+            AssertStages(
+                stages,
+                "{ $match : { '_id' : 1 } }",
+                "{ $project : { T : { $concat : ['$A', ';', '$B', ';', '$C'] }, _id : 0 } }");
+
+            var result = queryable.Single();
+            result.T.Should().Be("A1;B1;C1");
+        }
+
+        private IMongoCollection<Data> CreateCollection()
+        {
+            var collection = GetCollection<Data>("test");
+            CreateCollection(
+                collection,
+                new Data { Id = 1, A = "A1", B = "B1", C = "C1", D="D1" },
+                new Data { Id = 2, A = "A2", B = "B2", C = "C2", D="D2" });
+            return collection;
+        }
+
+        private class Data
+        {
+            public int Id { get; set; }
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+            public string D { get; set; }
+        }
+    }
+}
