2b0af3e82d1c38cdfda781c3e9e8b64b01b16e2d
BorisDog <BorisDog@users.noreply.github.com>
2023-08-01 19:01:07 -0700

    CSHARP-4675: Ignore __safecontent__ field in deserialization

---
+++ tests/MongoDB.Bson.Tests/Jira/CSharp4675Tests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using MongoDB.Bson.Serialization;
+using Xunit;
+
+namespace MongoDB.Bson.Tests.Jira
+{
+    public class CSharp4675Tests
+    {
+        public class A
+        {
+            public int X { get; set; }
+        }
+
+        [Fact]
+        public void SafeContent_element_should_be_ignored()
+        {
+            var subject = BsonSerializer.Deserialize<A>("{ X : 1, \"__safeContent__\": [] }");
+
+            subject.X.Should().Be(1);
+        }
+    }
+}
