0d73d468beb4997769db2dd978551efd9e50a6ef
Samir Boulema <samir@auto.nl>
2023-08-02 23:58:52 +0200

    Resolve comments from BorisDog

--- tests/MongoDB.Driver.Tests/PipelineDefinitionBuilderTests.cs
+++ tests/MongoDB.Driver.Tests/PipelineDefinitionBuilderTests.cs
@@ -195,6 +195,19 @@ namespace MongoDB.Driver.Tests
         }
 
         [Fact]
+        public void Search_should_add_expected_stage_with_sort()
+        {
+            var pipeline = new EmptyPipelineDefinition<BsonDocument>();
+            var builder = new SearchDefinitionBuilder<BsonDocument>();
+            var sortBuilder = new SortDefinitionBuilder<BsonDocument>();
+
+            var result = pipeline.Search(builder.Text("bar", "foo"), new() { Sort = sortBuilder.Ascending("foo") });
+
+            var stages = RenderStages(result, BsonDocumentSerializer.Instance);
+            stages[0].Should().Be("{ $search: { text: { query: 'foo', path: 'bar' }, sort: { 'foo': 1 } } }");
+        }
+
+        [Fact]
         public void Search_should_add_expected_stage_with_tracking()
         {
             var pipeline = new EmptyPipelineDefinition<BsonDocument>();
@@ -211,19 +224,6 @@ namespace MongoDB.Driver.Tests
 
             var stages = RenderStages(result, BsonDocumentSerializer.Instance);
             stages[0].Should().Be("{ $search: { text: { query: 'foo', path: 'bar' }, tracking: { searchTerms: 'foo' } } }");
-        }
-
-        [Fact]
-        public void Search_should_add_expected_stage_with_sort()
-        {
-            var pipeline = new EmptyPipelineDefinition<BsonDocument>();
-            var builder = new SearchDefinitionBuilder<BsonDocument>();
-            var sortBuilder = new SortDefinitionBuilder<BsonDocument>();
-
-            var result = pipeline.Search(builder.Text("bar", "foo"), new() { Sort = sortBuilder.Ascending("foo") });
-
-            var stages = RenderStages(result, BsonDocumentSerializer.Instance);
-            stages[0].Should().Be("{ $search: { text: { query: 'foo', path: 'bar' }, sort: { 'foo': 1 } } }");
         }
 
         [Fact]
