968323c18c57f1f9a66d781c2a162e3507ca510d
adelinowona <adelinowona@gmail.com>
2023-10-20 18:00:05 -0400

    Cleanup scripts

--- evergreen/run-perf-tests.sh
+++ evergreen/run-perf-tests.sh
@@ -3,14 +3,9 @@
 # Don't trace since the URI contains a password that shouldn't show up in the logs
 set -o errexit  # Exit the script with error if any of the commands fail
 
-# Supported/used environment variables:
-#       MONGODB_URI             Set the URI, including username/password to use to connect to the server
-
 ############################################
 #            Main Program                  #
 ############################################
-
-#export MONGODB_URI="${MONGODB_URI}"
 
 # Download the data to be used in the performance tests
 ./download-data.sh .
