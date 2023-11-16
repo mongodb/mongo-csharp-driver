e6d56fbf3d894c9a6b8e67a34c1ba540ff2aa030
adelinowona <adelinowona@gmail.com>
2023-11-16 16:42:47 -0500

    more path issues fixed

--- evergreen/run-perf-tests.sh
+++ evergreen/run-perf-tests.sh
@@ -7,6 +7,6 @@ set -o errexit  # Exit the script with error if any of the commands fail
 ############################################
 
 # Download the data to be used in the performance tests
-./download-data.sh .
+./scripts/download-data.sh .
 
 dotnet run -c Release -- --evergreen --filter '*'
