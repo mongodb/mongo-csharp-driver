c2c7731180f2501b64f62eb119b5186e03b6a899
Sergei Lipin <prchaoz@gmail.com>
2023-12-03 01:29:39 +0400

    CSHARP-1893: Code cleanup

--- src/MongoDB.Driver/MongoCollectionImpl.cs
+++ src/MongoDB.Driver/MongoCollectionImpl.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Xml.Serialization;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
