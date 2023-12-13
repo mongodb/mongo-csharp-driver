f14d2a4328c111375d1b8054e534aaa779f447bd
James Kovacs <jkovacs@post.harvard.edu>
2023-12-13 10:14:35 -0500

    Updated SslStreamSettings ctor test.

--- tests/MongoDB.Driver.Core.Tests/Core/Configuration/SslStreamSettingsTests.cs
+++ tests/MongoDB.Driver.Core.Tests/Core/Configuration/SslStreamSettingsTests.cs
@@ -34,7 +34,7 @@ namespace MongoDB.Driver.Core.Configuration
             subject.CheckCertificateRevocation.Should().BeFalse();
             subject.ClientCertificates.Should().BeEmpty();
             subject.ClientCertificateSelectionCallback.Should().BeNull();
-            subject.EnabledSslProtocols.Should().Be(SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
+            subject.EnabledSslProtocols.Should().Be((SslProtocols)12288 | SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls);
             subject.ServerCertificateValidationCallback.Should().BeNull();
         }
 
