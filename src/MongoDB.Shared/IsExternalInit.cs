2baa81755f0a7c6f12b6d3c01b9512222ceced5b
BorisDog <boris.dogad@gmail.com>
2024-02-29 13:41:54 -0800

    - Cleanup

--- src/MongoDB.Shared/IsExternalInit.cs
+++ src/MongoDB.Shared/IsExternalInit.cs
@@ -4,15 +4,4 @@ namespace System.Runtime.CompilerServices
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     internal class IsExternalInit { }
-
-    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-    internal sealed class CallerArgumentExpressionAttribute : Attribute
-    {
-        public CallerArgumentExpressionAttribute(string parameterName)
-        {
-            ParameterName = parameterName;
-        }
-
-        public string ParameterName { get; }
-    }
 }
