2ed1b5bd1eef9f58969e60beb4c2db31489d7a7c
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2024-03-26 16:02:10 -0700

    CSHARP-4979: Gossip cluster time from internal MongoClient to session entities

--- tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedEntityMap.cs
+++ tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedEntityMap.cs
@@ -281,6 +281,14 @@ namespace MongoDB.Driver.Tests.UnifiedTestOperations
         }
 
         // public methods
+        public void AdjustSessionsClusterTime(BsonDocument clusterTime)
+        {
+            foreach (var session in _sessions.Values)
+            {
+                session.WrappedCoreSession.AdvanceClusterTime(clusterTime);
+            }
+        }
+
         public void Dispose()
         {
             if (!_disposed)
