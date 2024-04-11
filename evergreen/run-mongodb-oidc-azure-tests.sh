63c8785b501c51f172f3d89f52e79c782cf05130
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2024-04-11 11:13:44 -0700

    ff

--- evergreen/run-mongodb-oidc-azure-tests.sh
+++ evergreen/run-mongodb-oidc-azure-tests.sh
@@ -11,14 +11,7 @@ echo "Installing .NET LTS SDK..."
 bash ./dotnet-install.sh --channel 6.0 --install-dir "$DOTNET_SDK_PATH" --no-path
 export PATH=$PATH:$DOTNET_SDK_PATH
 
-echo "test variables"
-
 source ./env.sh
-cat ./env.sh
-
 MONGODB_URI="mongodb://${OIDC_ADMIN_USER}:${OIDC_ADMIN_PWD}@${MONGODB_URI:10}?authSource=admin"
-
-echo "Final MongoUri:"
-echo $MONGODB_URI
 
 dotnet test --no-build --framework net6.0 --filter Category=MongoDbOidc -e OIDC_ENV=azure -e TOKEN_RESOURCE="${AZUREOIDC_RESOURCE}" -e MONGODB_URI="${MONGODB_URI}" --results-directory ./build/test-results --logger "console;verbosity=detailed" ./tests/MongoDB.Driver.Tests/bin/Debug/net6.0/MongoDB.Driver.Tests.dll
