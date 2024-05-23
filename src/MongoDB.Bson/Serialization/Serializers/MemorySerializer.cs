5d1cd4b3e2b225a5050383459155b9ba57aa03d9
BorisDog <boris.dogad@gmail.com>
2024-05-23 14:19:58 -0700

    CSHARP-4807: Support serialization of Memory<T> and ReadOnlyMemory<T>

---
+++ src/MongoDB.Bson/Serialization/Serializers/MemorySerializer.cs
@@ -0,0 +1,307 @@
+﻿/* Copyright 2010-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MongoDB.Bson.IO;
+
+namespace MongoDB.Bson.Serialization.Serializers
+{
+    /// <summary>
+    /// Represents a serializer for <see cref="ReadOnlyMemory{TItem}"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the item. Only primitive numeric types are supported.</typeparam>
+    public sealed class ReadonlyMemorySerializer<TItem> : MemorySerializerBase<TItem, ReadOnlyMemory<TItem>>
+        where TItem : struct
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadonlyMemorySerializer{TItem}" /> class.
+        /// </summary>
+        public ReadonlyMemorySerializer() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadonlyMemorySerializer{TItem}" /> class.
+        /// </summary>
+        public ReadonlyMemorySerializer(BsonType representation) : base(representation)
+        {
+        }
+
+        /// <summary>
+        /// Returns a serializer that has been reconfigured with the specified representation.
+        /// </summary>
+        /// <param name="representation">The representation.</param>
+        /// <returns>The reconfigured serializer.</returns>
+        public override MemorySerializerBase<TItem, ReadOnlyMemory<TItem>> WithRepresentation(BsonType representation)
+        {
+            if (representation == Representation)
+            {
+                return this;
+            }
+            else
+            {
+                return new ReadonlyMemorySerializer<TItem>(representation);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override ReadOnlyMemory<TItem> CreateMemory(TItem[] items) => items;
+
+        /// <inheritdoc/>
+        protected override Memory<TItem> GetMemory(ReadOnlyMemory<TItem> memory) => MemoryMarshal.AsMemory(memory);
+    }
+
+    /// <summary>
+    /// Represents a serializer for <see cref="Memory{TItem}"/>.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the item. Only primitive numeric types are supported.</typeparam>
+    public sealed class MemorySerializer<TItem> : MemorySerializerBase<TItem, Memory<TItem>>
+        where TItem : struct
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemorySerializer{TItem}" /> class.
+        /// </summary>
+        public MemorySerializer() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemorySerializer{TItem}" /> class.
+        /// </summary>
+        public MemorySerializer(BsonType representation) : base(representation)
+        {
+        }
+
+        /// <summary>
+        /// Returns a serializer that has been reconfigured with the specified representation.
+        /// </summary>
+        /// <param name="representation">The representation.</param>
+        /// <returns>The reconfigured serializer.</returns>
+        public override MemorySerializerBase<TItem, Memory<TItem>> WithRepresentation(BsonType representation)
+        {
+            if (representation == Representation)
+            {
+                return this;
+            }
+            else
+            {
+                return new MemorySerializer<TItem>(representation);
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override Memory<TItem> CreateMemory(TItem[] items) => items;
+
+        /// <inheritdoc/>
+        protected override Memory<TItem> GetMemory(Memory<TItem> memory) => memory;
+    }
+
+    /// <summary>
+    /// Represents an abstract base class for <see cref="Memory{TItem}"/> and <see cref="ReadOnlyMemory{TItem}"/> serializers.
+    /// </summary>
+    /// <typeparam name="TItem">The type of the item. Only primitive numeric types are supported.</typeparam>
+    /// <typeparam name="TMemory">The type of the memory struct.</typeparam>
+    public abstract class MemorySerializerBase<TItem, TMemory> : StructSerializerBase<TMemory>, IRepresentationConfigurable<MemorySerializerBase<TItem, TMemory>>
+        where TMemory : struct
+        where TItem : struct
+    {
+        private static readonly bool __isByte;
+        private static readonly Func<IBsonReader, TItem[]> __readItems;
+        private static readonly Action<IBsonWriter, Memory<TItem>> __writeItems;
+
+        static MemorySerializerBase()
+        {
+            switch (typeof(TItem))
+            {
+                case var t when t == typeof(sbyte):
+                    __readItems = reader => PrimitivesArrayReader.ReadInt8(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<sbyte>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteInt8(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(byte):
+                    __isByte = true;
+                    __readItems = reader => PrimitivesArrayReader.ReadUInt8(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<byte>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteUInt8(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(short):
+                    __readItems = reader => PrimitivesArrayReader.ReadInt16(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<short>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteInt16(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(ushort):
+                    __readItems = reader => PrimitivesArrayReader.ReadUInt16(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<ushort>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteUInt16(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(int):
+                    __readItems = reader => PrimitivesArrayReader.ReadInt32(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<int>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteInt32(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(uint):
+                    __readItems = reader => PrimitivesArrayReader.ReadUInt32(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<uint>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteUInt32(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(long):
+                    __readItems = reader => PrimitivesArrayReader.ReadInt64(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<long>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteInt64(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(ulong):
+                    __readItems = reader => PrimitivesArrayReader.ReadUInt64(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<ulong>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteUInt64(writer, span);
+                    };
+                    break;
+                case var t when t == typeof(float):
+                    __readItems = reader => PrimitivesArrayReader.ReadSingles(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<float>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteSingles(writer, span);
+                    };
+
+                    break;
+                case var t when t == typeof(double):
+                    __readItems = reader => PrimitivesArrayReader.ReadDoubles(reader) as TItem[];
+                    __writeItems = (writer, memory) =>
+                    {
+                        var span = Unsafe.As<Memory<TItem>, Memory<double>>(ref memory).Span;
+                        PrimitivesArrayWriter.WriteDoubles(writer, span);
+                    };
+                    break;
+                default:
+                    throw new NotSupportedException($"Not supported memory type {typeof(TItem)}. Only primitive numeric types are supported.");
+            };
+        }
+
+        /// <inheritdoc/>
+        public BsonType Representation { get; }
+
+        // constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemorySerializerBase{TItem, TMemory}" /> class.
+        /// </summary>
+        public MemorySerializerBase(BsonType representation)
+        {
+            if (representation != BsonType.Array &&
+                !(__isByte && representation == BsonType.Binary))
+            {
+                throw new ArgumentOutOfRangeException(nameof(representation));
+            }
+
+            Representation = representation;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemorySerializerBase{TItem, TMemory}" /> class.
+        /// </summary>
+        public MemorySerializerBase() :
+            this(__isByte ? BsonType.Binary : BsonType.Array) // Match the serialization behavior for arrays
+        {
+        }
+
+        // public methods
+        /// <inheritdoc/>
+        public override TMemory Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var reader = context.Reader;
+
+            var bsonType = reader.GetCurrentBsonType();
+            switch (bsonType)
+            {
+                case BsonType.Array:
+                    var items = __readItems(reader);
+
+                    return CreateMemory(items);
+                case BsonType.Binary:
+                    if (!__isByte)
+                    {
+                        throw CreateCannotDeserializeFromBsonTypeException(bsonType);
+                    }
+                    var bytes = reader.ReadBytes();
+                    return CreateMemory(bytes as TItem[]);
+                default:
+                    throw CreateCannotDeserializeFromBsonTypeException(bsonType);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TMemory value)
+        {
+            var memory = GetMemory(value);
+
+            switch (Representation)
+            {
+                case BsonType.Array:
+                    __writeItems(context.Writer, memory);
+                    break;
+                case BsonType.Binary:
+                    var bytes = MemoryMarshal.AsBytes(memory.Span);
+                    context.Writer.WriteBytes(bytes.ToArray());
+                    break;
+                default:
+                    throw new NotSupportedException(nameof(Representation));
+            }
+        }
+
+        /// <inheritdoc/>
+        public abstract MemorySerializerBase<TItem, TMemory> WithRepresentation(BsonType representation);
+
+        // explicit interface implementations
+        IBsonSerializer IRepresentationConfigurable.WithRepresentation(BsonType representation) =>
+            WithRepresentation(representation);
+
+        /// <summary>
+        /// Creates the Memory{TITem} structure.
+        /// </summary>
+        /// <param name="items">The items to initialize the resulting instance with.</param>
+        /// <returns>The created memory structure.</returns>
+        protected abstract TMemory CreateMemory(TItem[] items);
+
+        /// <summary>
+        /// Get the memory structure from TMemory instance.
+        /// </summary>
+        /// <returns>The Memory{TITem} structure.</returns>
+        protected abstract Memory<TItem> GetMemory(TMemory memory);
+    }
+}
---
+++ src/MongoDB.Bson/Serialization/Serializers/PrimitivesArrayReader.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using MongoDB.Bson.IO;
+
+namespace MongoDB.Bson.Serialization.Serializers
+{
+    internal static class PrimitivesArrayReader
+    {
+        private enum ConversionType
+        {
+            DoubleToSingle,
+            DoubleToDouble,
+            Int32ToInt8,
+            Int32ToUInt8,
+            Int32ToInt16,
+            Int32ToUInt16,
+            Int32ToInt32,
+            Int32ToUInt32,
+            Int64ToInt64,
+            Int64ToUInt64
+        }
+
+        public static byte[] ReadInt8(IBsonReader bsonReader) =>
+            ReadBsonArray<byte>(bsonReader, ConversionType.Int32ToInt8);
+
+        public static sbyte[] ReadUInt8(IBsonReader bsonReader) =>
+            ReadBsonArray<sbyte>(bsonReader, ConversionType.Int32ToUInt8);
+
+        public static short[] ReadInt16(IBsonReader bsonReader) =>
+            ReadBsonArray<short>(bsonReader, ConversionType.Int32ToInt16);
+
+        public static ushort[] ReadUInt16(IBsonReader bsonReader) =>
+            ReadBsonArray<ushort>(bsonReader, ConversionType.Int32ToUInt16);
+
+        public static int[] ReadInt32(IBsonReader bsonReader) =>
+            ReadBsonArray<int>(bsonReader, ConversionType.Int32ToInt32);
+
+        public static int[] ReadUInt32(IBsonReader bsonReader) =>
+            ReadBsonArray<int>(bsonReader, ConversionType.Int32ToUInt32);
+
+        public static float[] ReadSingles(IBsonReader bsonReader) =>
+            ReadBsonArray<float>(bsonReader, ConversionType.DoubleToSingle);
+
+        public static double[] ReadDoubles(IBsonReader bsonReader) =>
+            ReadBsonArray<double>(bsonReader, ConversionType.DoubleToDouble);
+
+        public static long[] ReadInt64(IBsonReader bsonReader) =>
+            ReadBsonArray<long>(bsonReader, ConversionType.Int64ToInt64);
+
+        public static ulong[] ReadUInt64(IBsonReader bsonReader) =>
+            ReadBsonArray<ulong>(bsonReader, ConversionType.Int64ToUInt64);
+
+        private static T[] ReadBsonArray<T>(
+            IBsonReader bsonReader,
+            ConversionType conversionType)
+        {
+            var (bsonDataType, bsonDataSize) = GetBsonDataTypeAndSize(conversionType);
+
+            var array = bsonReader.ReadRawBsonArray();
+            using var buffer = ThreadStaticBuffer.RentBuffer(array.Length);
+
+            var bytes = buffer.Bytes;
+            array.GetBytes(0, bytes, 0, array.Length);
+
+            var result = new List<T>();
+
+            var index = 4; // 4 first bytes are array object size in bytes
+            var maxIndex = array.Length - 1;
+
+            while (index < maxIndex)
+            {
+                ValidateBsonType(bsonDataType);
+
+                // Skip name
+                while (bytes[index] != 0) { index++; };
+                index++; // Skip string terminating 0
+
+                T value = default;
+
+                // Read next item
+                switch (conversionType)
+                {
+                    case ConversionType.DoubleToSingle:
+                        {
+                            var from = BitConverter.ToDouble(bytes, index);
+                            var to = (float)from;
+
+                            value = Unsafe.As<float, T>(ref to);
+                            break;
+                        }
+                    case ConversionType.DoubleToDouble:
+                        {
+                            var v = BitConverter.ToDouble(bytes, index);
+                            value = Unsafe.As<double, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int32ToInt8:
+                        {
+                            var v = (sbyte)BitConverter.ToInt32(bytes, index);
+                            value = Unsafe.As<sbyte, T>(ref v);
+
+                            break;
+                        }
+                    case ConversionType.Int32ToUInt8:
+                        {
+                            var v = (byte)BitConverter.ToInt32(bytes, index);
+                            value = Unsafe.As<byte, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int32ToInt16:
+                        {
+                            var v = (short)BitConverter.ToInt32(bytes, index);
+                            value = Unsafe.As<short, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int32ToUInt16:
+                        {
+                            var v = (ushort)BitConverter.ToInt32(bytes, index);
+                            value = Unsafe.As<ushort, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int32ToInt32:
+                        {
+                            var v = BitConverter.ToInt32(bytes, index);
+                            value = Unsafe.As<int, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int32ToUInt32:
+                        {
+                            var v = BitConverter.ToUInt32(bytes, index);
+                            value = Unsafe.As<uint, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int64ToInt64:
+                        {
+                            var v = BitConverter.ToInt64(bytes, index);
+                            value = Unsafe.As<long, T>(ref v);
+                            break;
+                        }
+                    case ConversionType.Int64ToUInt64:
+                        {
+                            var v = BitConverter.ToUInt64(bytes, index);
+                            value = Unsafe.As<ulong, T>(ref v);
+                            break;
+                        }
+                    default:
+                        throw new InvalidOperationException();
+                }
+
+                result.Add(value);
+
+                index += bsonDataSize;
+            }
+
+            ValidateBsonType(BsonType.EndOfDocument);
+
+            return result.ToArray();
+
+            void ValidateBsonType(BsonType bsonType)
+            {
+                if ((BsonType)bytes[index] != bsonType)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        private static (BsonType, int) GetBsonDataTypeAndSize(ConversionType conversionType) =>
+            conversionType switch
+            {
+                ConversionType.DoubleToSingle or
+                ConversionType.DoubleToDouble => (BsonType.Double, 8),
+
+                ConversionType.Int32ToUInt8 or
+                ConversionType.Int32ToInt8 or
+                ConversionType.Int32ToUInt16 or
+                ConversionType.Int32ToInt16 or
+                ConversionType.Int32ToInt32 or
+                ConversionType.Int32ToUInt32 => (BsonType.Int32, 4),
+
+                ConversionType.Int64ToInt64 or
+                ConversionType.Int64ToUInt64 => (BsonType.Int64, 8),
+
+                _ => throw new NotSupportedException()
+            };
+    }
+}
