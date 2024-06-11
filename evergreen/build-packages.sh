5bfcb79b577c204bdfeb47b033f2765c4f41257b
BorisDog <boris.dogad@gmail.com>
2024-06-11 09:57:51 -0700

    CSHARP-5050: Sign release artifacts or tags with MongoDB-managed keys

--- evergreen/build-packages.sh
+++ evergreen/build-packages.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit the script with error if any of the commands fail
 
+# Environment variables used as input:
+# PACKAGE_VERSION
+
 if [ -z "$PACKAGE_VERSION" ]; then
   PACKAGE_VERSION=$(bash ./evergreen/get-version.sh)
   echo Calculated PACKAGE_VERSION value: "$PACKAGE_VERSION"
