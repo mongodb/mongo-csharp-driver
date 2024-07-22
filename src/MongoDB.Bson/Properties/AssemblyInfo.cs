8ce30e54fd4b60971a2a0ee1e985ecd61f7f8a98
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2024-07-22 13:10:26 -0700

    PR

--- src/MongoDB.Bson/Properties/AssemblyInfo.cs
+++ src/MongoDB.Bson/Properties/AssemblyInfo.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 using MongoDB.Bson;
 
 [assembly: CLSCompliant(true)]
