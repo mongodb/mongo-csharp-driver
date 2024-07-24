2f5e234a5555674505c82ba9657aa71c1a73fedd
BorisDog <BorisDog@users.noreply.github.com>
2024-07-24 10:21:14 -0700

    CSHARP-5176: Migrate signing to Garasign (#1387)

--- evergreen/sign-packages.sh
+++ evergreen/sign-packages.sh
@@ -4,22 +4,15 @@ set -o errexit  # Exit the script with error if any of the commands fail
 # Environment variables used as input:
 # ARTIFACTORY_PASSWORD
 # ARTIFACTORY_USERNAME
-# AZURE_NUGET_SIGN_TENANT_ID
-# AZURE_NUGET_SIGN_CLIENT_ID
-# AZURE_NUGET_SIGN_CLIENT_SECRET
+# GRS_USERNAME
+# GRS_PASSWORD
 # PACKAGE_VERSION
 
 echo "${ARTIFACTORY_PASSWORD}" | docker login --password-stdin --username "${ARTIFACTORY_USERNAME}" artifactory.corp.mongodb.com
 
-docker run --platform="linux/amd64" --rm -v $(pwd):/workdir -w /workdir \
-  artifactory.corp.mongodb.com/release-tools-container-registry-local/azure-keyvault-nuget \
-  NuGetKeyVaultSignTool sign "artifacts/nuget/*.$PACKAGE_VERSION.nupkg" \
-    --force \
-    --file-digest=sha256 \
-    --timestamp-rfc3161=http://timestamp.digicert.com \
-    --timestamp-digest=sha256 \
-    --azure-key-vault-url=https://mdb-authenticode.vault.azure.net \
-    --azure-key-vault-tenant-id="$AZURE_NUGET_SIGN_TENANT_ID" \
-    --azure-key-vault-client-secret="$AZURE_NUGET_SIGN_CLIENT_SECRET" \
-    --azure-key-vault-client-id="$AZURE_NUGET_SIGN_CLIENT_ID" \
-    --azure-key-vault-certificate=authenticode-2021
+echo "GRS_CONFIG_USER1_USERNAME=${GRS_USERNAME}" >> "signing-envfile"
+echo "GRS_CONFIG_USER1_PASSWORD=${GRS_PASSWORD}" >> "signing-envfile"
+
+docker run --platform="linux/amd64" --env-file=signing-envfile --rm -v $(pwd):/workdir -w /workdir \
+  artifactory.corp.mongodb.com/release-tools-container-registry-local/garasign-jsign \
+  /bin/bash -c "jsign --tsaurl "http://timestamp.digicert.com" -a mongo-authenticode-2021 "./artifacts/nuget/*.$PACKAGE_VERSION.nupkg""
