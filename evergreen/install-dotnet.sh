5da97808c5eb542fb99b8ae13ec4be7bf012794b
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2024-09-10 11:25:02 -0700

    CSHARP-5268: Configure EF Core build to be run inside of Driver's project (#1442)

--- evergreen/install-dotnet.sh
+++ evergreen/install-dotnet.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit the script with error if any of the commands fail
 
-DOTNET_SDK_PATH=./.dotnet
-mkdir -p "$DOTNET_SDK_PATH"
+DOTNET_SDK_PATH="${DOTNET_SDK_PATH:-./.dotnet}"
 
 if [[ $OS =~ [Ww]indows.* ]]; then
   echo "Downloading Windows .NET SDK installer into $DOTNET_SDK_PATH folder..."
-  curl -Lfo "$DOTNET_SDK_PATH"/dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
-  echo "Installing .NET LTS SDK..."
-  powershell.exe "$DOTNET_SDK_PATH"/dotnet-install.ps1 -Channel 8.0 -InstallDir "$DOTNET_SDK_PATH" -NoPath
+  curl -Lfo ./dotnet-install.ps1 https://dot.net/v1/dotnet-install.ps1
+  echo "Installing .NET 8.0 SDK..."
+  powershell.exe ./dotnet-install.ps1 -Channel 8.0 -InstallDir "$DOTNET_SDK_PATH" -NoPath
 else
   echo "Downloading .NET SDK installer into $DOTNET_SDK_PATH folder..."
-  curl -Lfo "$DOTNET_SDK_PATH"/dotnet-install.sh https://dot.net/v1/dotnet-install.sh
-  echo "Installing .NET LTS SDK..."
-  bash "$DOTNET_SDK_PATH"/dotnet-install.sh --channel 8.0 --install-dir "$DOTNET_SDK_PATH" --no-path
+  curl -Lfo ./dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+  echo "Installing .NET 8.0 SDK..."
+  bash ./dotnet-install.sh --channel 8.0 --install-dir "$DOTNET_SDK_PATH" --no-path
 fi
--- evergreen/run-external-script.sh
+++ evergreen/run-external-script.sh
@@ -8,6 +8,7 @@ rm -rf "${LOCAL_PATH}"
 mkdir "${LOCAL_PATH}"
 cd "${LOCAL_PATH}" || exit
 
+echo "Cloning the remote repo..."
 git clone -b "${GIT_BRANCH:-main}" --single-branch "${GIT_REPO}" .
 
 # add/adjust nuget.config pointing to myget so intermediate versions could be restored
@@ -29,9 +30,11 @@ EOL
 fi
 
 # make files executable
+echo "Making files executable"
 for i in $(find "." -name \*.sh); do
   chmod +x $i
 done
 
 # execute the provided script
+echo "Evaluating the script"
 eval "$SCRIPT"
