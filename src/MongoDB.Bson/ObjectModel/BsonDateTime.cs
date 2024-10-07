3d5aef837e080c1d8ba51c8cf529ff95ae46bedf
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-10-07 16:57:07 +0200

    Removed unused method

--- src/MongoDB.Bson/ObjectModel/BsonDateTime.cs
+++ src/MongoDB.Bson/ObjectModel/BsonDateTime.cs
@@ -76,18 +76,6 @@ namespace MongoDB.Bson
             get { return _millisecondsSinceEpoch; }
         }
 
-        /// <summary>
-        /// Gets the DateTime value.
-        /// </summary>
-        [Obsolete("Use ToUniversalTime instead.")]
-        public DateTime Value
-        {
-            get
-            {
-                return BsonUtils.ToDateTimeFromMillisecondsSinceEpoch(_millisecondsSinceEpoch);
-            }
-        }
-
         // public operators
         /// <summary>
         /// Converts a DateTime to a BsonDateTime.
