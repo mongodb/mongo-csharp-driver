9bcb6800786a1fa701007a044ba0fc380db339dc
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-10-09 20:13:53 +0200

    Removed todo

--- src/MongoDB.Bson/ObjectModel/BsonBinarySubType.cs
+++ src/MongoDB.Bson/ObjectModel/BsonBinarySubType.cs
@@ -33,7 +33,7 @@ namespace MongoDB.Bson
         /// <summary>
         /// Obsolete binary data subtype (use Binary instead).
         /// </summary>
-        [Obsolete("Use Binary instead")]  //TODO Should we remove this?
+        [Obsolete("Use Binary instead")]
         OldBinary = 0x02,
         /// <summary>
         /// A UUID in a driver dependent legacy byte order.
