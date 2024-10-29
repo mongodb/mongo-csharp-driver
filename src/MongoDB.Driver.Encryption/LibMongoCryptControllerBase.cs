e44988934cffce913c17eedb402d5437c4f6ec24
adelinowona <adelinowona@gmail.com>
2024-10-29 13:30:27 -0400

    CSHARP-5360: Fix resource leaks in LibMongoCryptControllerBase::SendKmsRequest

--- src/MongoDB.Driver.Encryption/LibMongoCryptControllerBase.cs
+++ src/MongoDB.Driver.Encryption/LibMongoCryptControllerBase.cs
@@ -283,8 +283,9 @@ namespace MongoDB.Driver.Encryption
             var tlsStreamSettings = GetTlsStreamSettings(request.KmsProvider);
             var sslStreamFactory = new SslStreamFactory(tlsStreamSettings, _networkStreamFactory);
             using (var sslStream = sslStreamFactory.CreateStream(endpoint, cancellation))
+            using (var binary = request.Message)
             {
-                var requestBytes = request.Message.ToArray();
+                var requestBytes = binary.ToArray();
                 sslStream.Write(requestBytes, 0, requestBytes.Length);
 
                 while (request.BytesNeeded > 0)
@@ -305,8 +306,9 @@ namespace MongoDB.Driver.Encryption
             var tlsStreamSettings = GetTlsStreamSettings(request.KmsProvider);
             var sslStreamFactory = new SslStreamFactory(tlsStreamSettings, _networkStreamFactory);
             using (var sslStream = await sslStreamFactory.CreateStreamAsync(endpoint, cancellation).ConfigureAwait(false))
+            using (var binary = request.Message)
             {
-                var requestBytes = request.Message.ToArray();
+                var requestBytes = binary.ToArray();
                 await sslStream.WriteAsync(requestBytes, 0, requestBytes.Length).ConfigureAwait(false);
 
                 while (request.BytesNeeded > 0)
