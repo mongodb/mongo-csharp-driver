91daba729b27186f5ad2285b32fa5ea7354e147f
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-11-05 18:15:15 +0100

    Small fix plus added dictionary test

--- tests/MongoDB.Bson.Tests/Serialization/Conventions/EnumRepresentationConventionTests.cs
+++ tests/MongoDB.Bson.Tests/Serialization/Conventions/EnumRepresentationConventionTests.cs
@@ -33,6 +33,7 @@ namespace MongoDB.Bson.Tests.Serialization.Conventions
             public E E { get; set; }
             public E? NE { get; set; }
             public E[] ArrayEnum { get; set; }
+            public Dictionary<string, E> DictionaryEnum { get; set; }
             public int I { get; set; }
             public int[] ArrayInt { get; set; }
         }
@@ -55,7 +56,6 @@ namespace MongoDB.Bson.Tests.Serialization.Conventions
         [InlineData(BsonType.Int32)]
         [InlineData(BsonType.Int64)]
         [InlineData(BsonType.String)]
-        //TODO we need a test like this for collections of various kinds (including dictionaries)
         public void Apply_should_configure_serializer_when_member_is_a_nullable_enum(BsonType representation)
         {
             var subject = new EnumRepresentationConvention(representation);
@@ -76,6 +76,22 @@ namespace MongoDB.Bson.Tests.Serialization.Conventions
         {
             var subject = new EnumRepresentationConvention(representation);
             var memberMap = CreateMemberMap(c => c.ArrayEnum);
+
+            subject.Apply(memberMap);
+
+            var serializer = (IChildSerializerConfigurable)memberMap.GetSerializer();
+            var childSerializer = (EnumSerializer<E>)serializer.ChildSerializer;
+            childSerializer.Representation.Should().Be(representation);
+        }
+
+        [Theory]
+        [InlineData(BsonType.Int32)]
+        [InlineData(BsonType.Int64)]
+        [InlineData(BsonType.String)]
+        public void Apply_should_configure_serializer_when_member_is_an_enum_dictionary(BsonType representation)
+        {
+            var subject = new EnumRepresentationConvention(representation);
+            var memberMap = CreateMemberMap(c => c.DictionaryEnum);
 
             subject.Apply(memberMap);
 
