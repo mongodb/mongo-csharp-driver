0d3bc2396735f432e46f94dd6dea744ab96f3f49
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-12-10 15:05:13 -0500

    Small final correction

--- src/MongoDB.Bson/Serialization/Attributes/BsonDateOnlyOptionsAttribute.cs
+++ src/MongoDB.Bson/Serialization/Attributes/BsonDateOnlyOptionsAttribute.cs
@@ -61,7 +61,7 @@ namespace MongoDB.Bson.Serialization.Attributes
         /// <returns>A reconfigured serializer.</returns>
         protected override IBsonSerializer Apply(IBsonSerializer serializer)
         {
-            var reconfiguredSerializer = SerializerConfigurator.ReconfigureSerializer<DateOnlySerializer>(serializer, s => s.WithRepresentation(_representation, _documentFormat));
+            var reconfiguredSerializer = SerializerConfigurator.ReconfigureSerializer(serializer, (DateOnlySerializer s) => s.WithRepresentation(_representation, _documentFormat));
             return reconfiguredSerializer ?? base.Apply(serializer);
         }
     }
