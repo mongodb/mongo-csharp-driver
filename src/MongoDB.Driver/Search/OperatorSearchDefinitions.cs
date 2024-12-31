c7d2881bb5190ea079f01af388e5bbb01b780ed9
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2024-12-31 18:33:55 +0100

    Small corrections

--- src/MongoDB.Driver/Search/OperatorSearchDefinitions.cs
+++ src/MongoDB.Driver/Search/OperatorSearchDefinitions.cs
@@ -236,7 +236,7 @@ namespace MongoDB.Driver.Search
         private protected override BsonDocument RenderArguments(RenderArgs<TDocument> args) =>
             new("value", _values);
 
-        private static BsonValue ToBsonValue(TField value) =>
+        private static BsonValue ToBsonValue(TField value) =>  //TODO probably we need to change also this
             value switch
             {
                 bool v => (BsonBoolean)v,
@@ -351,43 +351,31 @@ namespace MongoDB.Driver.Search
     }
 
     internal sealed class RangeSearchDefinition<TDocument, TField> : OperatorSearchDefinition<TDocument>
+        where TField : struct, IComparable<TField>
     {
-        private readonly SearchRangeV2<TField> _range;
-        
+        private readonly SearchRange<TField> _range;
+        private readonly BsonValue _min;
+        private readonly BsonValue _max;
+
         public RangeSearchDefinition(
             SearchPathDefinition<TDocument> path,
-            SearchRangeV2<TField> range,
+            SearchRange<TField> range,
             SearchScoreDefinition<TDocument> score)
                 : base(OperatorType.Range, path, score)
         {
             _range = range;
+            _min = ToBsonValue(_range.Min);
+            _max = ToBsonValue(_range.Max);
         }
 
-        private protected override BsonDocument RenderArguments(RenderArgs<TDocument> args)
-        {
-            BsonValue min = null, max = null;
-            bool minInclusive = false, maxInclusive = false;
-            
-            if (_range.Min != null)
+        private protected override BsonDocument RenderArguments(RenderArgs<TDocument> args) =>
+            new()
             {
-                min = ToBsonValue(_range.Min.Value);
-                minInclusive = _range.Min.Inclusive;
-            }
-            
-            if (_range.Max != null)
-            {
-                max = ToBsonValue(_range.Max.Value);
-                maxInclusive = _range.Max.Inclusive;
-            }
-            
-            return new()
-            {
-                { minInclusive ? "gte" : "gt", min, min != null },
-                { maxInclusive ? "lte" : "lt", max, max != null }
+                { _range.IsMinInclusive ? "gte" : "gt", _min, _min != null },
+                { _range.IsMaxInclusive ? "lte" : "lt", _max, _max != null },
             };
-        }
-        
-        private static BsonValue ToBsonValue(TField value) =>
+
+        private static BsonValue ToBsonValue(TField? value) =>  //TODO Probably we need to change this too
             value switch
             {
                 sbyte v => (BsonInt32)v,
@@ -401,7 +389,6 @@ namespace MongoDB.Driver.Search
                 double v => (BsonDouble)v,
                 DateTime v => (BsonDateTime)v,
                 DateTimeOffset v => (BsonDateTime)v.UtcDateTime,
-                string v => (BsonString)v,
                 null => null,
                 _ => throw new InvalidCastException()
             };
