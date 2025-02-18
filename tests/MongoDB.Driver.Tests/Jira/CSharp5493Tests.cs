d2e3629d75983b9feb46ac6469f5827ae848b9be
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-02-18 19:25:44 +0100

    Added test

---
+++ tests/MongoDB.Driver.Tests/Jira/CSharp5493Tests.cs
@@ -0,0 +1,76 @@
+/* Copyright 2010-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Runtime.CompilerServices;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
+using Xunit;
+
+namespace MongoDB.Driver.Tests.Jira
+{
+    public class CSharp5493Tests
+    {
+        [Fact]
+        private void Test()
+        {
+            BsonSerializer.TryRegisterSerializer(new ObjectSerializer(_ => true));
+            var pack = new ConventionPack { new EnumRepresentationConvention(BsonType.String) };
+            ConventionRegistry.Register(
+                "enum",
+                pack,
+                t => true);
+
+            var client = DriverTestConfiguration.Client;
+            var database = client.GetDatabase("test");
+            database.DropCollection("recursiveClass");
+            var collection = database.GetCollection<RecursiveClass>("recursiveClass");
+
+            var toInsert = new RecursiveClass
+            {
+                Id = ObjectId.GenerateNewId(),
+                Name = "outer",
+                TestEnum = TestEnum.One,
+                Category = new RecursiveClass
+                {
+                    Id = ObjectId.GenerateNewId(),
+                    Name = "inner",
+                    TestEnum = TestEnum.Two
+                }
+            };
+            collection.InsertOne(toInsert);
+
+            var retrieved = collection.Find(c => c.Id == toInsert.Id).Single();
+            Assert.Equal(retrieved.Name, toInsert.Name);
+        }
+
+        public class RecursiveClass
+        {
+            [BsonId]
+            public ObjectId Id { get; set; }
+            public TestEnum TestEnum { get; set; }
+            public string Name { get; set; }
+            public RecursiveClass Category { get; set; }
+        }
+
+        public enum TestEnum
+        {
+            One,
+            Two,
+        }
+    }
+}
