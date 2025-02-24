0f21e7a018422dafabd839ee6e5a38ff3bfef953
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-02-24 10:01:28 +0100

    Correction to hedged reads deprecation

--- src/MongoDB.Driver/Core/Misc/Feature.cs
+++ src/MongoDB.Driver/Core/Misc/Feature.cs
@@ -311,7 +311,6 @@ namespace MongoDB.Driver.Core.Misc
         /// <summary>
         /// Gets the hedged reads feature.
         /// </summary>
-        [Obsolete("This property will be removed in a later release.")]
         public static Feature HedgedReads => __hedgedReads;
 
         /// <summary>
