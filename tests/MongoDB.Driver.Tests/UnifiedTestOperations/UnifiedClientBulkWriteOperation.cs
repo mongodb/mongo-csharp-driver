0ff5534fe5134bce7e17e588f04e851dbbce5106
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2025-04-02 16:13:20 -0700

    CSHARP-5530: Revise assertion for unacknowledged client bulkWrite result

--- tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedClientBulkWriteOperation.cs
+++ tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedClientBulkWriteOperation.cs
@@ -18,7 +18,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
 using MongoDB.Bson;
 
 namespace MongoDB.Driver.Tests.UnifiedTestOperations
