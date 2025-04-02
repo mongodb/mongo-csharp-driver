8e8eea591174620ebd071126f6f5e4526cef5f1b
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-04-02 13:21:00 +0200

    Small fix

--- src/MongoDB.Driver/Linq/Linq3Implementation/Translators/ExpressionToAggregationExpressionTranslators/MethodTranslators/ConvertMethodToAggregationExpressionTranslator.cs
+++ src/MongoDB.Driver/Linq/Linq3Implementation/Translators/ExpressionToAggregationExpressionTranslators/MethodTranslators/ConvertMethodToAggregationExpressionTranslator.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver.Linq.Linq3Implementation.Ast;
 using MongoDB.Driver.Linq.Linq3Implementation.Ast.Expressions;
 using MongoDB.Driver.Linq.Linq3Implementation.Misc;
@@ -25,14 +21,13 @@ namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToAggreg
                 throw new ExpressionNotSupportedException(expression);
             }
 
-            var fieldAst = ExpressionToAggregationExpressionTranslator.Translate(context, arguments[0]).Ast;
-
+            ByteOrder? byteOrder = null;
+            string format = null;
             AstExpression onErrorAst = null;
             AstExpression onNullAst = null;
             BsonBinarySubType? subType = null;
-            ByteOrder? byteOrder = null;
-            string format = null;
 
+            var fieldAst = ExpressionToAggregationExpressionTranslator.Translate(context, arguments[0]).Ast;
 
             var optionExpression = arguments[1];
 
@@ -54,7 +49,7 @@ namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToAggreg
                 format = options.Format;
                 byteOrder = options.ByteOrder;
             }
-            else if (arguments[1] is MemberInitExpression memberInitExpression)
+            else if (optionExpression is MemberInitExpression memberInitExpression)
             {
                 foreach (var binding in memberInitExpression.Bindings)
                 {
@@ -62,47 +57,54 @@ namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToAggreg
 
                     var memberName = memberAssignment.Member.Name;
 
-
-                    if (memberName == "OnError")
+                    switch (memberName)
                     {
-                        var translatedExpression =
-                            ExpressionToAggregationExpressionTranslator.Translate(context, memberAssignment.Expression);
-                        onErrorAst = translatedExpression.Ast;
-                    }
-                    else if (memberName == "OnNull")
-                    {
-                        var translatedExpression =
-                            ExpressionToAggregationExpressionTranslator.Translate(context, memberAssignment.Expression);
-                        onNullAst = translatedExpression.Ast;
-                    }
-                    else if (memberName == "Format")
-                    {
-                        if (memberAssignment.Expression is not ConstantExpression formatExpression)
+                        case "OnError":
                         {
-                            throw new ExpressionNotSupportedException(expression);  //TODO Improve
+                            onErrorAst = ExpressionToAggregationExpressionTranslator.Translate(context, memberAssignment.Expression).Ast;
+                            break;
                         }
-
-                        format = (string)formatExpression.Value;
-                    }
-                    else if (memberName == "ByteOrder")
-                    {
-                        if (memberAssignment.Expression is not ConstantExpression byteOrderExpression)
+                        case "OnNull":
                         {
-                            throw new ExpressionNotSupportedException(expression);  //TODO Improve
+                            onNullAst = ExpressionToAggregationExpressionTranslator.Translate(context, memberAssignment.Expression).Ast;
+                            break;
                         }
-
-                        byteOrder = (ByteOrder)byteOrderExpression.Value;
-                    }
-                    else if (memberName == "SubType")
-                    {
-                        if (memberAssignment.Expression is not ConstantExpression subTypeExpression)
+                        case "Format":
                         {
-                            throw new ExpressionNotSupportedException(expression);  //TODO Improve
-                        }
+                            if (memberAssignment.Expression is not ConstantExpression formatExpression)
+                            {
+                                throw new ExpressionNotSupportedException("The 'format' field must be a constant expression");  //TODO Improve message?
+                            }
 
-                        subType = (BsonBinarySubType)subTypeExpression.Value;
+                            format = (string)formatExpression.Value;
+                            break;
+                        }
+                        case "ByteOrder":
+                        {
+                            if (memberAssignment.Expression is not ConstantExpression byteOrderExpression)
+                            {
+                                throw new ExpressionNotSupportedException("The 'byteOrder' field must be a constant expression");  //TODO Improve message?
+                            }
+
+                            byteOrder = (ByteOrder)byteOrderExpression.Value;
+                            break;
+                        }
+                        case "SubType":
+                        {
+                            if (memberAssignment.Expression is not ConstantExpression subTypeExpression)
+                            {
+                                throw new ExpressionNotSupportedException("The 'subType' field must be a constant expression");  //TODO Improve message?
+                            }
+
+                            subType = (BsonBinarySubType)subTypeExpression.Value;
+                            break;
+                        }
                     }
                 }
+            }
+            else
+            {
+                throw new ExpressionNotSupportedException("The 'Options' argument can be either a constant expression or a member initialization expression");  //TODO Improve message?
             }
 
             var toType = method.GetGenericArguments()[1];
@@ -113,7 +115,7 @@ namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToAggreg
             return new TranslatedExpression(expression, ast, serializer);
         }
 
-        public static BsonType GetBsonType(Type type)  //TODO Do we have this kind of info somewhere else...?
+        private static BsonType GetBsonType(Type type)  //TODO Do we have this kind of info somewhere else...?
         {
             return Type.GetTypeCode(Nullable.GetUnderlyingType(type) ?? type) switch
             {
