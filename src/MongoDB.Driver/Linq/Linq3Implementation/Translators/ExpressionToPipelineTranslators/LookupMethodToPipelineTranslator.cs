0b92d63108611a8e3e48560d9a7e1d86aacbd990
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-05-27 09:54:42 +0200

    Added domain to MongoQueryProvider

--- src/MongoDB.Driver/Linq/Linq3Implementation/Translators/ExpressionToPipelineTranslators/LookupMethodToPipelineTranslator.cs
+++ src/MongoDB.Driver/Linq/Linq3Implementation/Translators/ExpressionToPipelineTranslators/LookupMethodToPipelineTranslator.cs
@@ -286,7 +286,7 @@ namespace MongoDB.Driver.Linq.Linq3Implementation.Translators.ExpressionToPipeli
             var localSymbol = context.CreateSymbol(localParameter, localAst, localSerializer);
             context = context.WithSymbol(localSymbol);
 
-            var provider = new MongoQueryProvider<TDocument>(documentSerializer, session: null, options: null);
+            var provider = new MongoQueryProvider<TDocument>(documentSerializer, session: null, options: null, serializationDomain: context.SerializationDomain);
             var queryable = new MongoQuery<TDocument, TDocument>(provider);
 
             body = ExpressionReplacer.Replace(body, queryableParameter, Expression.Constant(queryable));
