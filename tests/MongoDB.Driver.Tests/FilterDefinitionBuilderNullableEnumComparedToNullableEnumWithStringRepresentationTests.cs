01df61f81fbb5c158a60e7e15c4bfdd3e5a24573
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-07-03 17:43:42 +0200

    Various test fixes

--- tests/MongoDB.Driver.Tests/FilterDefinitionBuilderNullableEnumComparedToNullableEnumWithStringRepresentationTests.cs
+++ tests/MongoDB.Driver.Tests/FilterDefinitionBuilderNullableEnumComparedToNullableEnumWithStringRepresentationTests.cs
@@ -23,9 +23,7 @@ namespace MongoDB.Driver.Tests
 {
     public class FilterDefinitionBuilderNullableEnumComparedToNullableEnumWithStringRepresentationTests
     {
-        private static RenderArgs<C> __args = new(
-            BsonSerializer.DefaultSerializationDomain.SerializerRegistry.GetSerializer<C>(),
-            BsonSerializer.DefaultSerializationDomain);
+        private static RenderArgs<C> __args = new(BsonSerializer.SerializerRegistry.GetSerializer<C>(), BsonSerializer.SerializerRegistry);
         private static FilterDefinitionBuilder<C> __subject = Builders<C>.Filter;
 
         public enum E { A, B }
