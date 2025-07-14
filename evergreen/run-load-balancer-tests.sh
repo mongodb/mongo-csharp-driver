86335e10742529ba29514b6a62b370399621e033
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2025-07-14 15:07:23 -0700

    CSHARP-3504: Remove usages of xtrace in shell scripts

--- evergreen/run-load-balancer-tests.sh
+++ evergreen/run-load-balancer-tests.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-set -o xtrace  # Write all commands first to stderr
 set -o errexit # Exit the script with error if any of the commands fail
 
 # Supported/used environment variables:
