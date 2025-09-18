117276b6dad8af39cff5612f6b54f119a7e8f7b2
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-09-18 10:49:30 +0200

    Added comments

--- src/MongoDB.Driver/IClientSession.cs
+++ src/MongoDB.Driver/IClientSession.cs
@@ -21,9 +21,17 @@ using MongoDB.Driver.Core.Bindings;
 
 namespace MongoDB.Driver
 {
+    /// <summary>
+    /// //TODO
+    /// </summary>
     public static class ClientSessionExtensions
     {
         //TODO This will need to be moved somewhere else
+        /// <summary>
+        /// //TODO
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
         public static BsonTimestamp GetSnapshotTime(this IClientSessionHandle session)
         {
             return ((ClientSessionHandle)session).SnapshotTime;
