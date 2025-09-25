ee85c62c7be228b6c3b55c97983faca1ebe5781a
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-09-25 09:54:03 +0200

    Added test infrastructure

--- tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedEntityMap.cs
+++ tests/MongoDB.Driver.Tests/UnifiedTestOperations/UnifiedEntityMap.cs
@@ -1056,6 +1056,9 @@ namespace MongoDB.Driver.Tests.UnifiedTestOperations
                                 case "snapshot":
                                     options.Snapshot = option.Value.ToBoolean();
                                     break;
+                                case "snapshotTime":
+                                    options.SnapshotTime = _results[option.Value.AsString].AsBsonTimestamp;
+                                    break;
                                 case "causalConsistency":
                                     options.CausalConsistency = option.Value.ToBoolean();
                                     break;
