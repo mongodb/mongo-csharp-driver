23b6bd29f83ab51e2bd5df569f9fdee243211359
BorisDog <boris.dogadov@mongodb.com>
2025-11-24 16:11:28 -0800

    -Add temporary ADD_NET10_TFM flag

--- evergreen/run-unit-tests.sh
+++ evergreen/run-unit-tests.sh
@@ -7,5 +7,6 @@ if [ "$FRAMEWORK" = "netstandard2.1" ]; then
   FRAMEWORK="netcoreapp3.1"
 fi
 
+export ADD_NET10_TFM="1" # Remove after cake removal
 ./evergreen/compile-sources.sh
 dotnet test -c Release --no-build --filter "Category!=Integration"  -f "$FRAMEWORK" --results-directory ./build/test-results --logger  "junit;verbosity=detailed;LogFileName=TEST-{assembly}.xml;FailureBodyFormat=Verbose" --logger "console;verbosity=detailed"
