919058ec33a798a9af34234eb949f55531caa376
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2025-12-02 15:36:19 -0800

    pr

--- evergreen/run-atlas-connectivity-tests.sh
+++ evergreen/run-atlas-connectivity-tests.sh
@@ -8,4 +8,4 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 # Provision the correct connection string and set up SSL if needed
 ./evergreen/compile-sources.sh
-TEST_PROJECT_PATH=./**/AtlasConnectivity.Tests.csproj ./evergreen/execute-tests.sh
+TEST_PROJECT_PATH=./tests/**/AtlasConnectivity.Tests.csproj ./evergreen/execute-tests.sh
