1751c19533113276ed06f22b8d22566bc9dd0557
Ferdinando Papale <4850119+papafe@users.noreply.github.com>
2025-12-16 19:39:43 +0100

    Added TokenBucket plus basic tests

---
+++ src/MongoDB.Driver/TokenBucket.cs
@@ -0,0 +1,58 @@
+/* Copyright 2010-present MongoDB Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using MongoDB.Driver.Core.Misc;
+
+namespace MongoDB.Driver;
+
+internal sealed class TokenBucket
+{
+    // Default retry token capacity
+    private const double Capacity = 1000d;
+
+    private readonly object _lock = new();
+    private double _tokens = Capacity;
+
+    public double Tokens
+    {
+        get { lock (_lock) return _tokens; }
+    }
+
+    public bool Consume(double tokens)
+    {
+        Ensure.IsGreaterThan(tokens, 0, nameof(tokens));
+
+        lock (_lock)
+        {
+            if (_tokens >= tokens)
+            {
+                _tokens -= tokens;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public void Deposit(double tokens)
+    {
+        Ensure.IsGreaterThan(tokens, 0, nameof(tokens));
+
+        lock (_lock)
+        {
+            _tokens = Math.Min(_tokens + tokens, Capacity);
+        }
+    }
+}
