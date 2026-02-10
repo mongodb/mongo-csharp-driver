46640eac98f9dacf5231e16c8cfbcb2cc02d873e
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2026-02-09 17:13:49 -0800

    CSHARP-5873: Configure EF Core tests in scope of Driver's CI to use Net 10 SDK (#1873)

--- evergreen/install-dotnet.sh
+++ evergreen/install-dotnet.sh
@@ -8,38 +8,45 @@ echo "runtime: $FRAMEWORK"
 
 if [ -n "$FRAMEWORK" ]; then
   if [ "$FRAMEWORK" = "net5.0" ]; then
-    RUNTIME_VERSION="5.0"
+    RUNTIME_VERSIONS="5.0"
   elif [ "$FRAMEWORK" = "net6.0" ]; then
-    RUNTIME_VERSION="6.0"
+    RUNTIME_VERSIONS="6.0"
   elif [ "$FRAMEWORK" = "net8.0" ]; then
-    RUNTIME_VERSION="8.0"
+    RUNTIME_VERSIONS="8.0"
   elif [ "$FRAMEWORK" = "net10.0" ]; then
-    RUNTIME_VERSION="10.0"
+    RUNTIME_VERSIONS="10.0"
   elif [ "$FRAMEWORK" = "netstandard2.1" ]; then
-    RUNTIME_VERSION="3.1"
+    RUNTIME_VERSIONS="3.1"
   elif [ "$FRAMEWORK" = "netcoreapp3.1" ]; then
-    RUNTIME_VERSION="3.1"
+    RUNTIME_VERSIONS="3.1"
   fi
 fi
+
+RUNTIMES=()
+if [ -n "$RUNTIME_VERSIONS" ]; then
+  # Split RUNTIME_VERSIONS by comma into $RUNTIMES array
+  IFS=',' read -r -a RUNTIMES <<< "$RUNTIME_VERSIONS"
+fi
+
 
 if [[ $OS =~ [Ww]indows.* ]]; then
   echo "Downloading Windows .NET SDK installer into $DOTNET_ROOT folder..."
   curl -Lfo ./dotnet-install.ps1 https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.ps1
   echo "Installing .NET ${DOTNET_SDK_VERSION} SDK..."
   powershell.exe ./dotnet-install.ps1 -Channel "$DOTNET_SDK_VERSION" -InstallDir "$DOTNET_ROOT" -NoPath
-  if [ -n  "$RUNTIME_VERSION" ]; then
-    echo "Installing .NET ${RUNTIME_VERSION} runtime..."
-    powershell.exe ./dotnet-install.ps1 -Channel "$RUNTIME_VERSION" -Runtime dotnet -InstallDir "$DOTNET_ROOT" -NoPath
-  fi
+  for RUNTIME in "${RUNTIMES[@]}"; do
+    echo "Installing .NET ${RUNTIME} runtime..."
+    powershell.exe ./dotnet-install.ps1 -Channel "$RUNTIME" -Runtime dotnet -InstallDir "$DOTNET_ROOT" -NoPath
+  done
 else
   echo "Downloading .NET SDK installer into $DOTNET_ROOT folder..."
   curl -Lfo ./dotnet-install.sh https://builds.dotnet.microsoft.com/dotnet/scripts/v1/dotnet-install.sh
   echo "Installing .NET ${DOTNET_SDK_VERSION} SDK..."
   bash ./dotnet-install.sh --channel "$DOTNET_SDK_VERSION" --install-dir "$DOTNET_ROOT" --no-path
-  if [ -n  "$RUNTIME_VERSION" ]; then
-    echo "Installing .NET ${RUNTIME_VERSION} runtime..."
-    bash ./dotnet-install.sh --channel "$RUNTIME_VERSION" --runtime dotnet --install-dir "$DOTNET_ROOT" --no-path
-  fi
+  for RUNTIME in "${RUNTIMES[@]}"; do
+    echo "Installing .NET ${RUNTIME} runtime..."
+    bash ./dotnet-install.sh --channel "$RUNTIME" --runtime dotnet --install-dir "$DOTNET_ROOT" --no-path
+  done
 fi
 
 DOTNET_ROOT=$(cd "$DOTNET_ROOT" && pwd) # converts relative path to absolute
