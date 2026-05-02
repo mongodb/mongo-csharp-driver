c5086a1be23d7536e5e093824d3d42e2e36a4a9c
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2026-05-01 19:01:48 -0700

    pr

--- evergreen/push-packages.sh
+++ evergreen/push-packages.sh
@@ -25,7 +25,7 @@ execute_with_retry() {
     fi
 
     delay=$((attempt * 10))
-    echo "Command failed (attempt ${attempt} out of ${max_attempts}). Sleeping for ${delay} seconds..."
+    echo "Command failed (attempt ${attempt} out of ${max_attempts}). Sleeping for ${delay} seconds..." >&2
     sleep $delay
   done
 }
