41a6dc94b8fc664d63ff5d8a3427feef9c4ea26a
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2026-05-04 13:55:17 -0700

    Fix tests

--- tests/MongoDB.Driver.Tests/Specifications/sessions/SessionsProseTests.cs
+++ tests/MongoDB.Driver.Tests/Specifications/sessions/SessionsProseTests.cs
@@ -17,6 +17,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MongoDB.Bson;
