507dc324f9b5b714abaa2947556399486f241513
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2026-05-04 13:56:03 -0700

    pr

--- tests/MongoDB.Driver.Tests/Specifications/sessions/SessionsProseTests.cs
+++ tests/MongoDB.Driver.Tests/Specifications/sessions/SessionsProseTests.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright 2021-present MongoDB Inc.
+﻿/* Copyright 2010-present MongoDB Inc.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MongoDB.Bson;
@@ -387,7 +386,6 @@ namespace MongoDB.Driver.Tests.Specifications.sessions
             var mongoClient = DriverTestConfiguration.Client;
 
             using var session = mongoClient.StartSession(sessionOptions);
-
             var exception = Record.Exception(() => session.GetSnapshotTime());
             exception.Should().BeOfType<InvalidOperationException>();
             exception.Message.Should().Contain("non-snapshot session");
