0ae8ab2a19995695168a8717a7345035b4c6fe96
Oleksandr Poliakov <31327136+sanych-sun@users.noreply.github.com>
2026-07-24 15:45:58 -0700

    CSHARP-6139: Bump DocFx version to support C# 14 language features (#2082)

--- evergreen/build-apidocs.sh
+++ evergreen/build-apidocs.sh
@@ -10,7 +10,7 @@ echo "Configure dotnet cli to use local manifest"
 dotnet new tool-manifest --force
 
 echo "Installing docfx tool"
-dotnet tool install docfx --version "2.74.1" --local --verbosity q
+dotnet tool install docfx --version "2.78.5" --local --verbosity q
 
 echo "Building the api-docs"
 dotnet tool run docfx metadata ./apidocs/docfx.json --property ProduceReferenceAssembly=true
