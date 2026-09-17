c1df0a4cd9b3e6caef2ac6d0ead4eccc12a40f34
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2026-09-16 22:33:10 -0700

    pr

--- src/MongoDB.Driver/MongoClientBuilder.cs
+++ src/MongoDB.Driver/MongoClientBuilder.cs
@@ -27,8 +27,8 @@ using MongoDB.Driver.Core.Connections;
 using MongoDB.Driver.Core.Events;
 using MongoDB.Driver.Core.Misc;
 using MongoDB.Driver.Core.Operations;
-using MongoDB.Driver.Encryption;
 using MongoDB.Driver.Core.Servers;
+using MongoDB.Driver.Encryption;
 
 namespace MongoDB.Driver;
 
